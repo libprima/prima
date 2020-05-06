Bugfix: zero gradient in linear constraints and fixed bounds

python/interfaces/pdfo/_bobyqa.py
@@ -149,7 +149,7 @@ def bobyqa(fun, x0, args=(), bounds=None, options=None):
     # Preprocess the inputs.
     fun_c, x0_c, bounds_c, _, options_c, _, prob_info = prepdfo(fun, x0, args, bounds=bounds, options=options)
 
-    if prob_info['infeasible']:
+    if invoker != 'pdfo' and prob_info['infeasible']:
         # The problem turned out infeasible during prepdfo.
         exitflag = -4
         nf = 1
@@ -158,7 +158,7 @@ def bobyqa(fun, x0, args=(), bounds=None, options=None):
         fhist = np.array([fx], dtype=np.float64)
         constrviolation = prob_info['constrv_x0']
         chist = np.array([constrviolation], dtype=np.float64)
-    elif prob_info['nofreex']:
+    elif invoker != 'pdfo' and prob_info['nofreex']:
         # x was fixed by the bound constraints during prepdfo.
         exitflag = 13
         nf = 1

python/interfaces/pdfo/_cobyla.py
@@ -163,7 +163,7 @@ def cobyla(fun, x0, args=(), bounds=None, constraints=(), options=None):
     fun_c, x0_c, bounds_c, constraints_c, options_c, _, prob_info = \
         prepdfo(fun, x0, args, bounds=bounds, constraints=constraints, options=options)
 
-    if prob_info['infeasible']:
+    if invoker != 'pdfo' and prob_info['infeasible']:
         # The problem turned out infeasible during prepdfo.
         exitflag = -4
         nf = 1
@@ -173,7 +173,7 @@ def cobyla(fun, x0, args=(), bounds=None, constraints=(), options=None):
         constrviolation = prob_info['constrv_x0']
         chist = np.array([constrviolation], dtype=np.float64)
         output['nlc'] = prob_info['nlc_x0']
-    elif prob_info['nofreex']:
+    elif invoker != 'pdfo' and prob_info['nofreex']:
         # x was fixed by the bound constraints during prepdfo
         exitflag = 13
         nf = 1

python/interfaces/pdfo/_lincoa.py
@@ -158,7 +158,7 @@ def lincoa(fun, x0, args=(), bounds=None, constraints=(), options=None):
         warnings.warn(warn_message, Warning)
         output['warnings'].append(warn_message)
 
-    if prob_info['infeasible']:
+    if invoker != 'pdfo' and prob_info['infeasible']:
         # The problem turned out infeasible during prepdfo.
         exitflag = -4
         nf = 1
@@ -168,7 +168,7 @@ def lincoa(fun, x0, args=(), bounds=None, constraints=(), options=None):
         constrviolation = prob_info['constrv_x0']
         chist = np.array([constrviolation], dtype=np.float64)
         output['constr_modified'] = False
-    elif prob_info['nofreex']:
+    elif invoker != 'pdfo' and prob_info['nofreex']:
         # x was fixed by the bound constraints during prepdfo.
         exitflag = 13
         nf = 1