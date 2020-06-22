ba474f89d8951c9ee8e7170ae399dc6fb622fe67
zaikunzhang <zaikunzhang@gmail.com>
2020-06-23 01:55:00 +0800

    update infnan

---
+++ fsrc/test.f
@@ -0,0 +1,7 @@
+          program exponents
+            real(kind=4) :: x
+            real(kind=8) :: y
+          
+            print *, minexponent(x), maxexponent(x)
+            print *, minexponent(y), maxexponent(y)
+          end program exponents
