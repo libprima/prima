0c888eaf14879b209dda4d9c5995c0aa281287e2
zaikunzhang <zaikunzhang@gmail.com>
2020-06-23 23:58:11 +0800

    modulize bigden

--- fsrc/newuoa/bigden__genmod.f90
+++ fsrc/newuoa/bigden__genmod.f90
@@ -1,10 +1,10 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Tue Jun 23 11:35:15 2020
+        !COMPILER-GENERATED INTERFACE MODULE: Tue Jun 23 22:59:48 2020
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
-        MODULE BIGLAG__genmod
+        MODULE BIGDEN__genmod
           INTERFACE 
-            SUBROUTINE BIGLAG(N,NPT,XOPT,XPT,BMAT,ZMAT,IDZ,KNEW,DELTA,D,&
-     &ALPHA)
+            SUBROUTINE BIGDEN(N,NPT,XOPT,XPT,BMAT,ZMAT,IDZ,KOPT,KNEW,D, &
+     &WCHECK,VLAG,BETA)
               INTEGER(KIND=4), INTENT(IN) :: NPT
               INTEGER(KIND=4), INTENT(IN) :: N
               REAL(KIND=8), INTENT(IN) :: XOPT(N)
@@ -12,10 +12,12 @@
               REAL(KIND=8), INTENT(IN) :: BMAT(NPT+N,N)
               REAL(KIND=8), INTENT(IN) :: ZMAT(NPT,NPT-N-1)
               INTEGER(KIND=4), INTENT(IN) :: IDZ
+              INTEGER(KIND=4), INTENT(IN) :: KOPT
               INTEGER(KIND=4), INTENT(IN) :: KNEW
-              REAL(KIND=8), INTENT(IN) :: DELTA
-              REAL(KIND=8), INTENT(OUT) :: D(N)
-              REAL(KIND=8), INTENT(OUT) :: ALPHA
-            END SUBROUTINE BIGLAG
+              REAL(KIND=8), INTENT(INOUT) :: D(N)
+              REAL(KIND=8), INTENT(OUT) :: WCHECK(NPT+N)
+              REAL(KIND=8), INTENT(OUT) :: VLAG(NPT+N)
+              REAL(KIND=8), INTENT(OUT) :: BETA
+            END SUBROUTINE BIGDEN
           END INTERFACE 
-        END MODULE BIGLAG__genmod
+        END MODULE BIGDEN__genmod
