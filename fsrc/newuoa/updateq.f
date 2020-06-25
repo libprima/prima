165e9911f0b5c029846e38cf8e2e933b45ece719
zaikunzhang <zaikunzhang@gmail.com>
2020-06-26 02:05:56 +0800

    improve calquad; bigfix in updateq

--- fsrc/newuoa/updateq.f
+++ fsrc/newuoa/updateq.f
@@ -8,29 +8,32 @@
 
       real(kind = rp), intent(in) :: fqdiff, xptknew(n), bmatknew(n),   &
      & zmat(npt, npt - n - 1)
-      real(kind = rp), intent(inout) :: gq(n), hq(n), pq(n) 
+      real(kind = rp), intent(inout) :: gq(n), hq((n*(n+1))/2), pq(npt) 
 
-      integer :: i, ih, j, k
-      real(kind = rp) :: temp
+      integer :: i, ih, j
+      real(kind = rp) :: fqdz(npt - n - 1)
 
-      ! Update the explicit part of second derivatives.
+      ! Update the explicit part of second derivatives. It adds 
+      ! PQKNEW*outprod(XPTKNEW, XPTKNEW) to the explicit HESSIAN.
       ih = 0
       do i = 1, n
-          temp = pq(knew)*xptknew(i)
-          do j = 1, i
-              ih = ih + 1
-              hq(ih) = hq(ih) + temp*xptknew(j)
-          end do
+          hq(ih+1:ih+i)=hq(ih+1:ih+i)+(pq(knew)*xptknew(i))*xptknew(1:i)
+          ih = ih + i
       end do
       
       ! Update the implicit part of second derivatives.
+      fqdz = fqdiff*zmat(knew, :)
+      fqdz(1 : idz - 1) = -fqdz(1 : idz - 1)
       pq(knew) = zero
+      !----------------------------------------------------------------!
+      !!! The following DO LOOP implements the update given below, yet
+      !!! the result will not be exactly the same due to the
+      !!! non-associativity of floating point arithmetic addition.
+      !!! In future versions, we will use MATMUL instead of DO LOOP.
+!----!pq = pq + matmul(zmat, fqdz) !-----------------------------------!
+      !----------------------------------------------------------------!
       do j = 1, npt - n - 1
-          temp = fqdiff*zmat(knew, j)
-          if (j < idz) temp = -temp
-          do k = 1, npt
-              pq(k) = pq(k) + temp*zmat(k, j)
-          end do
+          pq = pq + fqdz(j)*zmat(:, j)
       end do
 
       ! Update the gradient.
