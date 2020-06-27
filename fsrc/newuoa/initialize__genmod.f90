54427025f22e817e0b1a56e596d035cebbe83460
zaikunzhang <zaikunzhang@gmail.com>
2020-06-27 16:50:46 +0800

    decouple thhe initialization of (XPT, FVAL) and (Q, BMAT, ZMAT)

--- fsrc/newuoa/initialize__genmod.f90
+++ fsrc/newuoa/initialize__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Sat Jun 27 00:16:39 2020
+        !COMPILER-GENERATED INTERFACE MODULE: Sat Jun 27 16:43:59 2020
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE INITIALIZE__genmod
