27b1668847ee3d28ab4ef6553ffa99360bd1e6ab
Zaikun <zaikunzhang@gmail.com>
2020-09-26 10:44:03 +0800

    2020-09-26-10:44:03-HKT back up

---
+++ fsrc/newuoa/newuoa_html/table_of_contents.js
@@ -0,0 +1,26 @@
+var ToC = `
+<div id="main-dropdown" class="dropdown" tabindex="0">
+<button id="dropdown-button" class="dropbtn"><span>Contents </span><img id="dropdown-icon" src="ToCdropdown.png" style="height:12px"></button>
+<div id="dropdown-buffer" class="dropdown-border" style="height:120px;width:300px;"></div><div id="nav-dropdown-content" class="dropdown-content"><a href="projectoverview.html">Overview</a>
+<a href="dictionary_A.html">Data Dictionary</a>
+<a href="contents_file_C.html">File Contents</a>
+<a href="progunit_xref_A.html">Program Unit Cross Reference</a>
+<a href="object_xref_C.html">Object Cross Reference</a>
+<a href="inc_xref_P.html">Include File Cross Reference</a>
+<a href="decltree_&.html">Declaration Tree</a>
+<a href="progunitcomp_metrics_D.html">Program Unit Complexity</a>
+<a href="projmetrics.html">Project Metrics</a>
+<a href="progunit_metrics_C.html">Program Unit Metrics</a>
+<a href="file_metrics_C.html">File Metrics</a>
+<a href="fileam_C.html">File Average Metrics</a>
+<a href="implicitdeclobjs_P.html">Implicitly Declared Objects</a>
+<a href="uninitializeditems_N.html">Uninitialized Items</a>
+<a href="unuseditemsall_C.html">Unused Objects and Functions</a>
+<a href="unusedobj_C.html">Unused Objects</a>
+<a href="unusedprogunit_L.html">Unused Program Units</a>
+<a href="entity_index.html">Index</a>
+<a href="entity_tree_index.html">Tree Index</a>
+
+</div>
+</div>
+`
