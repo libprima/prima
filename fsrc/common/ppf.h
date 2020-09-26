e65c6bdd28f8317dc5cc35082421a39d4e158bab
Zaikun <zaikunzhang@gmail.com>
2020-09-26 11:26:02 +0800

    2020-09-26-11:26:02-HKT back up

--- fsrc/common/ppf.h
+++ fsrc/common/ppf.h
@@ -1,7 +1,42 @@
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
 #if defined __DEBUGGING__
 #undef __DEBUGGING__
 #endif
 #define __DEBUGGING__ 1 
+
+
+
 
 
 #if defined __FORTRAN_STANDARD__
@@ -9,36 +44,92 @@
 #endif
 #define __FORTRAN_STANDARD__ 2003 
 
+
+
+
+
+
 #if defined __USE_INTRINSIC_ALGEBRA__
 #undef __USE_INTRINSIC_ALGEBRA__
 #endif
 #define __USE_INTRINSIC_ALGEBRA__ 1
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
 #if defined __USE_POWELL_ALGEBRA__
 #undef __USE_POWELL_ALGEBRA__
 #endif
 #define __USE_POWELL_ALGEBRA__ 1 
+
+
+
+
+
+ 
+ 
 #if defined __INTEGER_KIND__
 #undef __INTEGER_KIND__
 #endif
 #define __INTEGER_KIND__ 0 
+
+
+
+
+
+
+ 
+ 
+ 
 #if defined __REAL_PRECISION__
 #undef __REAL_PRECISION__
 #endif
 #define __REAL_PRECISION__ 64 
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
 #if defined __QP_AVAILABLE__
 #undef __QP_AVAILABLE__
 #endif
+
+ 
+ 
+ 
+ 
+ 
 #define __QP_AVAILABLE__ 0  
+
 
 #if __QP_AVAILABLE__ != 1 && __REAL_PRECISION__ > 64
 #undef __REAL_PRECISION__
 #define __REAL_PRECISION__ 64
 #endif
+
+
+
+
+
 #if defined __USE_IEEE_ARITHMETIC__
 #undef __USE_IEEE_ARITHMETIC__
 #endif
 #define __USE_IEEE_ARITHMETIC__ 0
 
+
+ 
 #if defined __GFORTRAN__
 #if __REAL_PRECISION__ <= 64 && __GNUC__ >= 5
 #undef __USE_IEEE_ARITHMETIC__
@@ -78,6 +169,11 @@
 #undef __USE_IEEE_ARITHMETIC__ 
 #define __USE_IEEE_ARITHMETIC__ 0
 #endif
+
+
+
+
+
 #if defined __USE_STORAGE_SIZE__
 #undef __USE_STORAGE_SIZE__
 #endif
@@ -122,6 +218,12 @@
 #undef __USE_STORAGE_SIZE__
 #define __USE_STORAGE_SIZE__ 0 
 #endif
+
+
+
+
+
+ 
 #if defined __USE_ISO_FORTRAN_ENV_INTREAL__
 #undef __USE_ISO_FORTRAN_ENV_INTREAL__
 #endif
@@ -166,3 +268,4 @@
 #undef __USE_ISO_FORTRAN_ENV_INTREAL__
 #define __USE_ISO_FORTRAN_ENV_INTREAL__ 0
 #endif
+
