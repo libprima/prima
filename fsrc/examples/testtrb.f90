cc38e38e94bfd3febbd81564f71812908ee82ffd
Zaikun <zaikunzhang@gmail.com>
2020-12-06 09:07:07 +0800

    2020-12-06-09:07:07-HKT stable
    Former-commit-id: c0317f96616ce2c4565287aa8cddd868fff31b39 [formerly 0b19b705976cfeb583b371368dcaffa226455140]
    Former-commit-id: 20be75c0c1ec842dc02e98d5465048e4d58c6d37

--- fsrc/examples/testtrb.f90
+++ fsrc/examples/testtrb.f90
@@ -3,6 +3,4 @@ program testtrb
 !    call tracebackqq(string='stop', user_exit_code=-1)
 !  CALL TRACEBACKQQ(STRING="Bad value for TEMP",USER_EXIT_CODE=123)
     call backtrace()
-    call abort
-    print '(/1A/)', 'Finish.'
 end program testtrb
