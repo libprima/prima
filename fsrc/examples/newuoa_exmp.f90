f21f263f586d22d94aa7a11893c42f421de7a0ee
Zaikun <zaikunzhang@gmail.com>
2021-04-20 23:48:30 +0800

    2021-04-20-23:48:30-HKT Fortran_Tips
    Former-commit-id: f98dbe32db835827a4e7d340dad280bb509ea53f [formerly 8fa14a0af4c391d48bef6ce35b3447d97b5ecc41]
    Former-commit-id: ea34bd7510c2e0dd98b5032974135b903bdab7c9

--- fsrc/examples/newuoa_exmp.f90
+++ fsrc/examples/newuoa_exmp.f90
@@ -1,6 +1,6 @@
-! This is an example to illustrate the usage of NEWUOA. 
-!   
-! Coded by Zaikun Zhang in July 2020 based on Powell's Fortran 77 code. 
+! This is an example to illustrate the usage of NEWUOA.
+!
+! Coded by Zaikun Zhang in July 2020 based on Powell's Fortran 77 code.
 
 
 !!!!!!!!!!!!!!!!!! THE MODULE THAT IMPLEMENTS CALFUN !!!!!!!!!!!!!!!!!!!
@@ -13,29 +13,29 @@ public :: calfun
 contains
 
     subroutine calfun (x, f)
-    ! The Chebyquad test problem (Fletcher, 1965) 
+    ! The Chebyquad test problem (Fletcher, 1965)
     implicit none
-    
-    real(kind(0.0D0)), intent(in) :: x(:)
-    real(kind(0.0D0)), intent(out) :: f
-    real(kind(0.0D0)) :: y(size(x) + 1, size(x) + 1), tmp
-    integer :: i, n
-    
+
+    real(kind(0.0D0)), intent(in):: x(:)
+    real(kind(0.0D0)), intent(out):: f
+    real(kind(0.0D0)):: y(size(x) + 1, size(x) + 1), tmp
+    integer:: i, n
+
     n = size(x)
-    
+
     y(1 : n, 1) = 1.0D0
     y(1 : n, 2) = 2.0D0*x - 1.0D0
     do i = 2, n
-        y(1: n, i + 1) = 2.0D0*y(1 : n, 2)*y(1 : n, i) - y(1 : n, i - 1)
+        y(1: n, i+1) = 2.0D0*y(1 : n, 2)*y(1 : n, i) - y(1 : n, i-1)
     end do
-    
+
     f = 0.0D0
-    do i = 1, n + 1 
+    do i = 1, n+1
         tmp = sum(y(1 : n, i))/real(n, kind(0.0D0))
         if (mod(i, 2) /= 0) then
-            tmp = tmp + 1.0D0/real(i*i - 2*i, kind(0.0D0))
+            tmp = tmp+1.0D0/real(i*i - 2*i, kind(0.0D0))
         end if
-        f = f + tmp*tmp
+        f = f+tmp*tmp
     end do
     end subroutine calfun
 
@@ -59,30 +59,30 @@ use calfun_mod, only : calfun
 
 implicit none
 
-integer :: i, n, alloc_stat
-real(kind(0.0D0)) :: rhobeg, f
-real(kind(0.0D0)), allocatable :: x(:)
+integer:: i, n, alloc_stat
+real(kind(0.0D0)):: rhobeg, f
+real(kind(0.0D0)), allocatable:: x(:)
 
-! If CALFUN is provided as an external subroutine, then remove the line 
+! If CALFUN is provided as an external subroutine, then remove the line
 ! of "use calfun_mod, only : calfun", and uncomment the following line:
 !----------------------------------------------------------------------!
 !external calfun
 !----------------------------------------------------------------------!
 
 do n = 2, 10, 2
-    ! Sets up the initial X for the Chebyquad problem. 
+    ! Sets up the initial X for the Chebyquad problem.
     if (allocated(x)) deallocate(x)
     allocate(x(n), stat = alloc_stat)
     if (alloc_stat /= 0) print *, 'Memory allocation failed.'
     do i = 1, n
-        x(i) = real(i, kind(0.0D0))/real(n + 1, kind(0.0D0))
+        x(i) = real(i, kind(0.0D0))/real(n+1, kind(0.0D0))
     end do
 
     rhobeg = 0.2D0*x(1)
 
     print '(/1A, I2)', 'Result with N = ', n
 
-    ! The following line illustrates how to call NEWUOA. 
+    ! The following line illustrates how to call NEWUOA.
     !------------------------------------------------------------------!
     call newuoa(calfun, x, f, rhobeg = rhobeg, iprint = 2)
     !------------------------------------------------------------------!
