533f728692a8344e502135b7f3016f89c3cb5e09
Zaikun <zaikunzhang@gmail.com>
2021-05-26 01:00:44 +0800

    2021.05.26.01:00:44.HKT back up
    Former-commit-id: 7416a15a6dcbe126f5d4294763b717c4a17a9ee2 [formerly 3d6f366bba6388b11888ffdf2fb186d3da69f2e7]
    Former-commit-id: 1dcf44e4c9d0633bc5b2a5c47271f852f50d6e17

--- fsrc/uobyqa/main.f90
+++ fsrc/uobyqa/main.f90
@@ -2,29 +2,29 @@
 !
 !     The Chebyquad test problem (Fletcher, 1965) for N = 2,4,6,8.
 !
-      IMPLICIT NONE
+      implicit none
 !*--AA00018
 !*++
 !*++ Local variable declarations rewritten by SPAG
 !*++
-      INTEGER :: i , iprint , maxfun , n
-      REAL*8(R8KIND) :: rhobeg , rhoend
-      REAL*8(R8KIND) , DIMENSION(10000) :: w
-      REAL*8(R8KIND) , DIMENSION(10) :: x
+      integer :: i, iprint, maxfun, n, info
+      real*8(R8KIND) :: rhobeg, rhoend, f, ftarget
+      real*8(R8KIND), dimension(10000) :: w
+      real*8(R8KIND), dimension(10) :: x
 !*++
 !*++ End of declarations rewritten by SPAG
 !*++
       iprint = 2
       maxfun = 5000
       rhoend = 1.0D-8
-      DO n = 2 , 8 , 2
-         DO i = 1 , n
-            x(i) = DFLOAT(i)/DFLOAT(n+1)
-         ENDDO
-         rhobeg = 0.2D0*x(1)
-         PRINT 99001 , n
-99001    FORMAT (//5X,'******************'/5X,'Results with N =',I2,/5X,&
-     &           '******************')
-         CALL UOBYQA(n,x,rhobeg,rhoend,iprint,maxfun,w)
-      ENDDO
-      END PROGRAM AA0001
+      do n = 2, 8, 2
+          do i = 1, n
+              x(i) = DFLOAT(i) / DFLOAT(n + 1)
+          end do
+          rhobeg = 0.2D0 * x(1)
+          print 99001, n
+99001     format(//5X, '******************'/5X, 'Results with N =', I2, /5X,&
+      &           '******************')
+          call UOBYQA(n, x, rhobeg, rhoend, iprint, maxfun, w, f, info, ftarget)
+      end do
+      end program AA0001
