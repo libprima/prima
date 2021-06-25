1a5c3f3bee9b594964e519cbfe10e33f1cfab010
Zaikun <zaikunzhang@gmail.com>
2021-06-25 23:04:08 +0800

    2021.06.25.23:04:08.HKT back up

--- matlab/mex_gateways/prob.F90
+++ matlab/mex_gateways/prob.F90
@@ -1,12 +1,11 @@
-! PROB_MOD is a module defining the optimization problem. In particular,
-! it implements CALFUN and CALCFC.  CALFUN evaluates the objective function
-! for unconstrained, bound constrained, and linearly constrained problems;
-! CALCFC evaluates the objective function and constraint for nonlinearly
-! constrained prolems.
+! PROB_MOD is a module defining the optimization problem. In particular, it implements CALFUN and
+! CALCFC.  CALFUN evaluates the objective function for unconstrained, bound constrained, and
+! linearly constrained problems; CALCFC evaluates the objective function and constraint for
+! nonlinearly constrained prolems.
 !
 ! Coded by Zaikun Zhang in July 2020.
 !
-! Last Modified: Sunday, May 23, 2021 PM04:54:58
+! Last Modified: Friday, June 25, 2021 PM10:06:09
 
 
 #include "fintrf.h"
@@ -52,9 +51,8 @@ call fmxWriteMPtr(x, pinput(1))
 ! Call the MATLAB function that evaluates the objective function
 call fmxCallMATLAB(fun_ptr, pinput, poutput)
 
-! Verify the class and shape of outputs. Indeed, fmxReadMPtr
-! does also the verification. We do it here in order to print a
-! more informative error message when the verification fails.
+! Verify the class and shape of outputs. Indeed, fmxReadMPtr does also the verification. We do it
+! here in order to print a more informative error message when the verification fails.
 if (.not. fmxIsDoubleScalar(poutput(1))) then
     eid = 'PROBLEM:ObjectiveNotScalar'
     mssg = 'PROBLEM: Objective function does not return a scalar.'
@@ -69,10 +67,9 @@ if (funval > HUGEFUN .or. is_nan(funval)) then
     funval = HUGEFUN
 end if
 
-! Destroy the matrix created by fmxWriteMPtr for X.
-! This must be done. Otherwise, the matrix will be destroyed only
-! when the MEX function terminates. However, this subroutine will
-! be called maybe thousands of times before that.
+! Destroy the matrix created by fmxWriteMPtr for X. This must be done. Otherwise, the matrix will be
+! destroyed only when the MEX function terminates. However, this subroutine will be called maybe
+! thousands of times before that.
 call mxDestroyArray(pinput(1))
 
 end subroutine calfun
