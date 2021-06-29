534e6bd47eaa1c48cb409921d8e60761a825a525
Zaikun <zaikunzhang@gmail.com>
2021-06-29 17:04:18 +0800

    2021.06.29.17:04:18.HKT back up
    Former-commit-id: 95bfc48998d661d2f3748893f8fe968d7b11e49b [formerly 53e7cb4a8c7c482672cc7405a7f18ea5a252d4f0]
    Former-commit-id: 2259ebfb34d7d37b4b417dc76e4e7b130742db7c

---
+++ fsrc/common/logging.F90
@@ -0,0 +1,60 @@
+! logging_mod is a module providing some subroutines for logging.
+!
+! Coded by Zaikun Zhang in July 2020 based on Powell's Fortran 77 code and papers.
+!
+! Last Modified: Tuesday, June 29, 2021 PM05:01:40
+
+
+module logging_mod
+
+implicit none
+private
+public :: logging
+
+
+contains
+
+
+subroutine logging(nfo, iprint, nf, f, x, solver)
+use consts_mod, only : RP, IK, MSSGLEN, OUTUNIT
+use info_mod, only : FTARGET_ACHIEVED, MAXFUN_REACHED
+use info_mod, only : SMALL_TR_RADIUS, TRSUBP_FAILED
+use info_mod, only : NAN_X, NAN_INF_F, NAN_MODEL
+implicit none
+
+! Inputs
+integer(IK), intent(in) :: iprint
+integer(IK), intent(in) :: info
+integer(IK), intent(in) :: nf
+real(RP), intent(in) :: f
+real(RP), intent(in) :: x(:)
+character(len=*), intent(in) :: solver
+
+! Local variables
+integer :: ios  ! Should be an integer of default kind
+character(len=100) :: fout
+character(len=3) :: fstat
+character(len=MSSGLEN) :: mssg
+logical :: fexist
+
+
+fout = solver//'_output.txt'
+inquire (file=trim(fout), exist=fexist)
+if (fexist) then
+    fstat = 'old'
+else
+    fstat = 'new'
+end if
+open (unit=OUTUNIT, file=trim(fout), status=fstat, position='append', iostat=ios, action='write')
+if (ios /= 0) then
+    print '(1A)', 'Fail to open file '//trim(fout)//'!'
+else
+    write (OUTUNIT, '(/1A, I7, 4X, 1A, 1PD18.10, 4X, 1A, /(1P, 5D15.6))') 'Function number', nf, &
+        & 'F = ', f, 'The corresponding X is:', x
+    close (OUTUNIT)
+end if
+
+end subroutine fmssg
+
+
+end module logging_mod
