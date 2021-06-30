0eb3d65f160e4b5be284d6d0c0f26f07170d4591
Zaikun <zaikunzhang@gmail.com>
2021-06-30 15:04:01 +0800

    2021.06.30.15:04:01.HKT back up

--- fsrc/common/logging.F90
+++ fsrc/common/logging.F90
@@ -2,7 +2,7 @@
 !
 ! Coded by Zaikun Zhang in July 2020 based on Powell's Fortran 77 code and papers.
 !
-! Last Modified: Wednesday, June 30, 2021 PM12:34:42
+! Last Modified: Wednesday, June 30, 2021 PM02:45:38
 
 
 module logging_mod
@@ -15,43 +15,41 @@ public :: logging
 contains
 
 
-subroutine logging(lnnmb, nf, f, nx, conv, mssg)
-use consts_mod, only : RP, IK, MSSGLEN, OUTUNIT
-use info_mod, only : FTARGET_ACHIEVED, MAXFUN_REACHED
-use info_mod, only : SMALL_TR_RADIUS, TRSUBP_FAILED
-use info_mod, only : NAN_X, NAN_INF_F, NAN_MODEL
+subroutine logging(srname, lnnum, nf, f, x, con, mssg)
+use consts_mod, only : RP, IK, MSSGLEN
 implicit none
 
 ! Inputs
-integer(IK), intent(in) :: iprint
-integer(IK), intent(in) :: info
+integer(IK), optional, intent(in) :: lnnum
 integer(IK), intent(in) :: nf
 real(RP), intent(in) :: f
 real(RP), intent(in) :: x(:)
-character(len=*), intent(in) :: solver
+real(RP), optional, intent(in) :: con(:)
+character(len=*), optional, intent(in) :: srname 
+character(len=*), optional, intent(in) :: mssg
 
 ! Local variables
+integer(IK), parameter :: LOGUNIT = 11
 integer :: ios  ! Should be an integer of default kind
 character(len=100) :: fout
 character(len=3) :: fstat
-character(len=MSSGLEN) :: mssg
 logical :: fexist
 
 
-fout = solver//'_output.txt'
+fout = solver//'_log.txt'
 inquire (file=trim(fout), exist=fexist)
 if (fexist) then
     fstat = 'old'
 else
     fstat = 'new'
 end if
-open (unit=OUTUNIT, file=trim(fout), status=fstat, position='append', iostat=ios, action='write')
+open (unit=LOGUNIT, file=trim(fout), status=fstat, position='append', iostat=ios, action='write')
 if (ios /= 0) then
     print '(1A)', 'Fail to open file '//trim(fout)//'!'
 else
-    write (OUTUNIT, '(/1A, I7, 4X, 1A, 1PD18.10, 4X, 1A, /(1P, 5D15.6))') 'Function number', nf, &
+    write (LOGUNIT, '(/1A, 1A, I7, 4X, 1A, 1PD18.10, 4X, 1A, /(1P, 5D15.6))') 'Line number', lnnum, 'Function number', nf, &
         & 'F = ', f, 'The corresponding X is:', x
-    close (OUTUNIT)
+    close (LOGUNIT)
 end if
 
 end subroutine fmssg
