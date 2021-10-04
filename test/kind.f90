6bba72ab79a6c70c464768107fb9712e0a13bd0f
Zaikun <zaikunzhang@gmail.com>
2021-10-04 15:52:01 +0800

    improve the Makefile for tests: compile the code for all cases before testing the binaries

--- test/kind.f90
+++
@@ -1,6 +0,0 @@
-module kind_mod
-implicit none
-private
-public :: DP
-integer, parameter :: DP = kind(0.0D0)
-end module kind_mod
--- test/testieee.f90
+++
@@ -1,6 +0,0 @@
-! Oracle Fortran compiler 12.4 crashes when compiling this code.
-program testieee
-    use ieee_arithmetic, only : my_nan => ieee_is_nan
-    implicit none
-    print *, my_nan(0.0)
-end program testieee
