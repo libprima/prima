31b7e527def9ecbc283c347579e1c7e0183ca784
Zaikun <zaikunzhang@gmail.com>
2021-11-28 20:22:08 +0800

    211128.202208.HKT for the moment, blacklist some time-consuming problems for the verification of cobyla
    Former-commit-id: 58d027559e6e0bfaa2e57e7526b591e6c09d62e4 [formerly f8277c40f037c17c31ff765f224ff62fc13e0f13]
    Former-commit-id: 0b4ca0d1da02dab910028bdce07267650a5d5748

---
+++ matlab/tests/timing.m
@@ -0,0 +1,18 @@
+function [tlist, plist] = timing(maxdim)
+
+req.mindim = 1;
+req.maxdim = maxdim;
+req.maxcon = 500*maxdim;
+req.type='ubln';
+plist = secup(req);
+tlist= NaN(length(plist), 1);
+
+for ip = 1 : length(plist)
+    plist{ip}
+    tic;
+    cobylan(macup(plist{ip}));
+    tlist(ip) = toc;
+    tlist(ip)
+end
+
+save('timing.mat', 'plist', 'tlist');
