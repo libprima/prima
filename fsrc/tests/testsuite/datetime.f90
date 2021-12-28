8d6edaa47e9af92bef2de029b84adea3b5b97e95
Zaikun <zaikunzhang@gmail.com>
2021-12-28 14:53:59 +0800

    211228.145359.HKT improve WEEK()
    Former-commit-id: 68744c42d124dde84aac3bed296b5f5fffab2b62 [formerly 473cde0b692b3ded0ac87f513c2e30115767a792]
    Former-commit-id: 444eea36cd94ce112ec52d1df0dcea3581feee24

--- fsrc/tests/testsuite/datetime.f90
+++ fsrc/tests/testsuite/datetime.f90
@@ -3,47 +3,59 @@ implicit none
 private
 public :: year
 public :: week
-public :: isleap
 
 
 contains
 
 
-function year() result(y)
+function year(ymd) result(y)
 !--------------------------------------------------------------------------------------------------!
 ! This function returns the number of the current year.
 !--------------------------------------------------------------------------------------------------!
 implicit none
+! Inputs
+integer, intent(in), optional :: ymd(3)
 ! Outputs
 integer :: y
 
 ! Local values
 integer :: values(8)
 
-call date_and_time(values=values)
-y = values(1)
+if (present(ymd)) then
+    y = ymd(1)
+else
+    call date_and_time(values=values)
+    y = values(1)
+end if
 end function year
 
 
-!function month() result(m)
-!--------------------------------------------------------------------------------------------------!
-! This function returns the number of the current month.
-!--------------------------------------------------------------------------------------------------!
+!function month(ymd) result(m)
+!!--------------------------------------------------------------------------------------------------!
+!! This function returns the number of the current month.
+!!--------------------------------------------------------------------------------------------------!
 !implicit none
+!! Inputs
+!integer, intent(in), optional :: ymd(3)
 !! Outputs
 !integer :: m
 
 !! Local values
 !integer :: values(8)
 
-!call date_and_time(values=values)
-!m = values(2)
+!if (present(ymd)) then
+!    m = ymd(2)
+!else
+!    call date_and_time(values=values)
+!    m = values(2)
+!end if
 !end function month
 
 
 function day(ymd) result(d)
 !--------------------------------------------------------------------------------------------------!
-! This function returns the number of the current day in the current year.
+! This function returns the number of the day YMD(3) in the year YMD(1) or the current day in the
+! current year.
 !--------------------------------------------------------------------------------------------------!
 implicit none
 ! Inputs
@@ -53,35 +65,31 @@ integer :: d
 
 ! Local values
 integer :: dom
-integer :: monthday(12)
+integer :: monlens(12)
 integer :: moy
 integer :: values(8)
-integer :: year
+integer :: y
 
 if (present(ymd)) then
-    year = ymd(1)
+    y = ymd(1)
     moy = ymd(2)
     dom = ymd(3)
 else
     call date_and_time(values=values)
-    year = values(1)
+    y = values(1)
     moy = values(2)
     dom = values(3)
 end if
 
-if (isleap(year)) then
-    monthday = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-else
-    monthday = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-end if
-d = sum(monthday(1:moy - 1)) + dom
+monlens = month_lengths(y)
+d = sum(monlens(1:moy - 1)) + dom
 end function day
 
 
 function week(ymd) result(w)
 !--------------------------------------------------------------------------------------------------!
 ! This function returns the number of the current week in the current year.
-! N.B.: This function increments W every 7 days starting from January 1, not necessarily on Monday.
+! N.B.: This function takes the week of January 1 as the first week and increments W every MONDAY.
 !--------------------------------------------------------------------------------------------------!
 implicit none
 ! Inputs
@@ -89,17 +97,37 @@ integer, intent(in), optional :: ymd(3)
 ! Outputs
 integer :: w
 
+! Local variables
+integer :: jan1
+integer :: dom
+integer :: moy
+integer :: values(8)
+integer :: y
+
 if (present(ymd)) then
-    w = ceiling(real(day(ymd)) / 7.0)
+    y = ymd(1)
+    moy = ymd(2)
+    dom = ymd(3)
 else
-    w = ceiling(real(day()) / 7.0)
+    call date_and_time(values=values)
+    y = values(1)
+    moy = values(2)
+    dom = values(3)
+end if
+
+jan1 = whatday([y, 1, 1])
+
+if (present(ymd)) then
+    w = ceiling(real(day(ymd) + jan1 - 1) / 7.0)
+else
+    w = ceiling(real(day() + jan1 - 1) / 7.0)
 end if
 end function week
 
 
 function isleap(y) result(is_leap)
 !--------------------------------------------------------------------------------------------------!
-! This function tests whether the current year is a leap year.
+! This function tests whether the year Y or the current year is a leap year.
 !--------------------------------------------------------------------------------------------------!
 implicit none
 ! Inputs
@@ -118,5 +146,95 @@ end if
 
 is_leap = (mod(y_loc, 4) == 0 .and. mod(y_loc, 100) /= 0) .or. (mod(y_loc, 400) == 0)
 end function isleap
+
+
+function days_since_20000101(ymd) result(d)
+!--------------------------------------------------------------------------------------------------!
+! This function returns the number of days since 2000-01-01 until YMD or the current day.
+!--------------------------------------------------------------------------------------------------!
+implicit none
+! Inputs
+integer, intent(in), optional :: ymd(3)
+! Outputs
+integer :: d
+
+integer :: dom
+integer :: moy
+integer :: values(12)
+integer :: y
+
+if (present(ymd)) then
+    y = ymd(1)
+    moy = ymd(2)
+    dom = ymd(3)
+else
+    call date_and_time(values=values)
+    y = values(1)
+    moy = values(2)
+    dom = values(3)
+end if
+
+d = 0
+if (y > 2000) then
+    do y = 2000, y - 1
+        if (isleap(y)) then
+            d = d + 366
+        else
+            d = d + 365
+        end if
+    end do
+elseif (y < 2000) then
+    do y = y, 1999
+        if (isleap(y)) then
+            d = d - 366
+        else
+            d = d - 365
+        end if
+    end do
+end if
+d = d + day(ymd) - 1
+end function days_since_20000101
+
+
+function whatday(ymd) result(d)
+!--------------------------------------------------------------------------------------------------!
+! This function returns the day of week of the day YMD(3) or the current day, 1 for Monday, and
+! 7 for Sunday.
+! N.B.: 2000-01-01 is Saturday.
+!--------------------------------------------------------------------------------------------------!
+implicit none
+! Inputs
+integer, intent(in), optional :: ymd(3)
+! Outputs
+integer :: d
+
+if (present(ymd)) then
+    d = mod(5 + days_since_20000101(ymd), 7) + 1
+else
+    d = mod(5 + days_since_20000101(), 7) + 1
+end if
+if (d <= 0) then
+    d = d + 7
+end if
+end function whatday
+
+
+function month_lengths(y) result(monlens)
+!--------------------------------------------------------------------------------------------------!
+! This function returns the lengths of months of the year Y or the current year.
+!--------------------------------------------------------------------------------------------------!
+implicit none
+! Inputs
+integer, intent(in), optional :: y
+! Outputs
+integer :: monlens(12)
+
+if (isleap(y)) then
+    monlens = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+else
+    monlens = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+end if
+end function month_lengths
+
 
 end module datetime_mod
