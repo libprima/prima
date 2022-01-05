7a1c3911a30838bd3d8ee14beea66dc75fbcf3e3
Zaikun <zaikunzhang@gmail.com>
2022-01-05 20:15:51 +0800

    220105.201551.HKT add pre/post-conditions to circle.f90
    Former-commit-id: f695807226a4aa369ed480dcf09ad99e4a88eb00 [formerly af9ebe442e8c36ebfd4d368cd59884ce58b7e51d]
    Former-commit-id: 7b6300e227fbc514ff2f510d52a5d59b495fe1b6

--- fsrc/common/circle.f90
+++ fsrc/common/circle.f90
@@ -18,7 +18,8 @@ contains
 
 
 function circle_min(fun, args, grid_size) result(angle)
-use, non_intrinsic :: consts_mod, only : RP, IK, ZERO, TWO, HALF, PI!, DEBUGGING
+use, non_intrinsic :: consts_mod, only : RP, IK, ZERO, TWO, HALF, PI, DEBUGGING
+use, non_intrinsic :: debug_mod, only : assert
 use, non_intrinsic :: infnan_mod, only : is_nan
 implicit none
 
@@ -31,7 +32,7 @@ integer(IK), intent(in) :: grid_size
 real(RP) :: angle
 
 ! Local variables
-!character(len=*), parameter :: srname = 'CIRCLE_MIN'
+character(len=*), parameter :: srname = 'CIRCLE_MIN'
 integer(IK) :: i
 integer(IK) :: iopt
 real(RP) :: angles(grid_size)
@@ -42,13 +43,24 @@ real(RP) :: fval(grid_size)
 real(RP) :: step
 real(RP) :: unit_angle
 
+! Preconditions
+if (DEBUGGING) then
+    call assert(grid_size >= 3, 'GRID_SIZE >= 3', srname)
+end if
+
+!====================!
+! Calculation starts !
+!====================!
+
 unit_angle = (TWO * PI) / real(grid_size, RP)
 angles = unit_angle * real([(i, i=1, grid_size)] - 1, RP)
 fval = [(fun(angles(i), args), i=1, grid_size)]
+
 if (all(is_nan(fval))) then
     angle = ZERO
     return
 end if
+
 iopt = int(minloc(fval, mask=(.not. is_nan(fval)), dim=1) - 1, IK)
 fopt = fval(iopt + 1)
 fa = fval(modulo(iopt - 1_IK, grid_size) + 1)
@@ -61,11 +73,22 @@ else
     step = ZERO
 end if
 angle = unit_angle * (real(iopt, RP) + step)  ! It may not be in [0, 2*PI].
+
+!====================!
+!  Calculation ends  !
+!====================!
+
+! Postconditions
+if (DEBUGGING) then
+    call assert(.not. is_nan(angle), 'ANGLE is not NaN', srname)
+end if
+
 end function circle_min
 
 
 function circle_maxabs(fun, args, grid_size) result(angle)
-use, non_intrinsic :: consts_mod, only : RP, IK, ZERO, TWO, HALF, PI!, DEBUGGING
+use, non_intrinsic :: consts_mod, only : RP, IK, ZERO, TWO, HALF, PI, DEBUGGING
+use, non_intrinsic :: debug_mod, only : assert
 use, non_intrinsic :: infnan_mod, only : is_nan
 implicit none
 
@@ -78,7 +101,7 @@ integer(IK), intent(in) :: grid_size
 real(RP) :: angle
 
 ! Local variables
-!character(len=*), parameter :: srname = 'CIRCLE_MAXABS'
+character(len=*), parameter :: srname = 'CIRCLE_MAXABS'
 integer(IK) :: i
 integer(IK) :: iopt
 real(RP) :: angles(grid_size)
@@ -89,13 +112,24 @@ real(RP) :: fval(grid_size)
 real(RP) :: step
 real(RP) :: unit_angle
 
+! Preconditions
+if (DEBUGGING) then
+    call assert(grid_size >= 3, 'GRID_SIZE >= 3', srname)
+end if
+
+!====================!
+! Calculation starts !
+!====================!
+
 unit_angle = (TWO * PI) / real(grid_size, RP)
 angles = unit_angle * real([(i, i=1, grid_size)] - 1, RP)
 fval = [(fun(angles(i), args), i=1, grid_size)]
+
 if (all(is_nan(fval))) then
     angle = ZERO
     return
 end if
+
 iopt = int(maxloc(abs(fval), mask=(.not. is_nan(fval)), dim=1) - 1, IK)
 fopt = fval(iopt + 1)
 fa = fval(modulo(iopt - 1_IK, grid_size) + 1)
@@ -108,6 +142,12 @@ else
     step = ZERO
 end if
 angle = unit_angle * (real(iopt, RP) + step)  ! It may not be in [0, 2*PI].
+
+! Postconditions
+if (DEBUGGING) then
+    call assert(.not. is_nan(angle), 'ANGLE is not NaN', srname)
+end if
+
 end function circle_maxabs
 
 end module circle_mod
