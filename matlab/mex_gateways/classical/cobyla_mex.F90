a95cbbbda1da1d8b9853b07d352fe3e37508ab6a
Zaikun <zaikunzhang@gmail.com>
2022-01-18 01:17:45 +0800

    220118.011745.HKT correct classical/cobyla_mex.F90, where two lines were removed accidentally
    Former-commit-id: d7569e4f11cb4d12dfc63c1847388623ecd46c05 [formerly 7f007b2ceb37d02dbeb3760c57f78d025ae6127d]
    Former-commit-id: 68dad407b75e3d66696ab2fd380b5f315b359076

--- matlab/mex_gateways/classical/cobyla_mex.F90
+++ matlab/mex_gateways/classical/cobyla_mex.F90
@@ -10,8 +10,12 @@
 !
 ! Started in March 2020
 !
-! Last Modified: Tuesday, January 18, 2022 AM12:57:35
+! Last Modified: Tuesday, January 18, 2022 AM01:15:53
 !--------------------------------------------------------------------------------------------------!
+
+#include "fintrf.h"
+
+module cobylacl_mod
 
 use, non_intrinsic :: fmxcl_mod, only : RP_CL, IK_CL
 implicit none
