89fd20d1f3cf7df4826a0ff4c5dda2b84f02afc2
Zaikun <zaikunzhang@gmail.com>
2022-02-09 15:55:20 +0800

    220209.155520.HKT improve the matlab tests

---
+++ matlab/tests/showpath.m
@@ -0,0 +1,16 @@
+function ndftpath = showpath()
+
+fprintf('\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n');
+fprintf('\nCurrently in %s\n', cd());
+
+fprintf('\nNon-default paths:\n');
+oldpath = path();
+restoredefaultpath;
+dftpath = path();
+path(oldpath);
+ndftpath = setdiff(split(oldpath, ':'), split(dftpath, ':'));
+for ic = 1 : length(ndftpath)
+    fprintf('\n%s', ndftpath{ic});
+end
+
+fprintf('\n\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n');
