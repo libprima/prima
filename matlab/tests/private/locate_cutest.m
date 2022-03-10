caa9d634882acb64d0ddf9937e433d8c6d2ab21d
Zaikun <zaikunzhang@gmail.com>
2022-03-10 22:54:19 +0800

    220310.225419.HKT revert the changes made for the tests; improve `macup` to avoid unncessary calculations of gradient and Hessian

--- matlab/tests/private/locate_cutest.m
+++ matlab/tests/private/locate_cutest.m
@@ -4,17 +4,14 @@ function cpaths = locate_cutest()
 
 cdir_dft = fullfile(homedir(), 'local', 'cutesif', 'cutest');
 
-%cdir = getenv('CUTEST');
-%if isempty(cdir)
-%    cdir = cdir_dft;
-%    setenv('CUTEST', cdir);  % This is needed by `cutestdir`, which will be called by `macup`.
-%end
+cdir = getenv('CUTEST');
+if isempty(cdir)
+    cdir = cdir_dft;
+    setenv('CUTEST', cdir);  % This is needed by `cutestdir`, which will be called by `macup`.
+end
 
-cmtools = fullfile(fileparts(cdir_dft), 'mtools', 'msrc');
-%cmtools = fullfile(fileparts(cdir), 'mtools', 'msrc');
+cmtools = fullfile(fileparts(cdir), 'mtools', 'msrc');
 cpaths = {cmtools};
-
-olddir=cd(fileparts(cmtools)); clear('setup'); setup; cd(olddir);
 
 for ip = 1 : length(cpaths)
     addpath(cpaths{ip});
