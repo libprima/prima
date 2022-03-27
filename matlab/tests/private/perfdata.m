9b2aa041c7a070658ea5bf9a7e1dd7640be0c69c
Zaikun <zaikunzhang@gmail.com>
2022-03-27 16:20:44 +0800

    220327.162044.HKT improve the matlab profiling scripts
    Former-commit-id: 3b63648cd4f31059f87a5987e6e7c9c9fc5d7bcb [formerly f443aca7e117f7646d064c2aa579a081e6c8b68f]
    Former-commit-id: 36902baf465c9d481b834a41dfac4c032bc5afbc

--- matlab/tests/private/perfdata.m
+++ matlab/tests/private/perfdata.m
@@ -1,25 +1,40 @@
-function summary = perfdata(solvers, options)
+function summary_file = perfdata(solvers, options)
 
-stamp = strcat(strjoin(solvers, '_'), '.', int2str(options.mindim), '_', int2str(options.maxdim), '.', options.type);
-time = options.time;
-if isfield(options, 'stamp') && (isa(options.stamp, 'char') || isa(options.stamp, 'string')) && ~isempty(options.stamp)
-    time = strcat(time, '.', options.stamp);
-end
 data_dir = options.data_dir;
-matfile = fullfile(data_dir, strcat(stamp, '.perfdata.mat'));
+test_feature = options.test_feature;
+feature_and_time = [test_feature, '.', options.time];
 
-loaded = false;
+% `stamp` is an identifier for the test. N.B.:
+% 1. `stamp` does not include `time`, so that it is easy to identify when we reload the data.
+% 2. `stamp` does not include `test_feature` (but the file names will), so that we can make
+%    `test_feature` appear after `perf_*`, which is converting when we view the files in terminal
+%    (when using tab for auto-completion, we do not need to type `test_feature` if the files in the
+%    current directory correspond to only one `test_feature`).
+stamp = strcat(strjoin(solvers, '_'), '.', int2str(options.mindim), '_', int2str(options.maxdim), '.', options.type);
+
+% `outdir` is the directory to contain the figures (.eps, .pdf) and problem list (problem.txt).
+outdir = fullfile(data_dir, strcat(stamp, '.', feature_and_time));
+if ~exist(outdir, 'dir')
+    mkdir(outdir);
+end
+
 % Check whether to reload data or calculate everything from scratch.
+matfile = fullfile(data_dir, strcat(stamp, '.perfdata.', test_feature, '.mat'));
 if (isfield(options, 'reload') && options.reload == true)
     % Use directly the data of the last computation for the same solvers and dimensions.
-    stamp_with_wildcard = strcat(strjoin(solvers, '_'), '.', int2str(options.mindim), '_', int2str(options.maxdim), '.*', options.type, '*');
-    matfile_with_wildcard = fullfile(data_dir, strcat(stamp_with_wildcard, '.perfdata.mat'));
+    % `stamp_with_wildcard` replaces `options.type` in `stamp` by a wildcard that contains `options.type`.
+    stamp_with_wildcard = regexprep(stamp, ['\.', options.type, '$'], ['\.\*', options.type, '\*']);
+    matfile_with_wildcard = strrep(matfile, stamp, stamp_with_wildcard);
     mlist = dir(matfile_with_wildcard);
     if ~isempty(mlist)
-        matfile = fullfile(mlist(1).folder, mlist(1).name);
+        % Take the latest .mat file that complies with the name pattern `matfile_with_wildcard`.
+        [~, im] = max([mlist(:).datenum]);
+        matfile = fullfile(mlist(im).folder, mlist(im).name);
+    else
+        error('Fail to load data, because no .mat file found with the name pattern \n\n%s', matfile_with_wildcard);
     end
     load(matfile, 'frec', 'fmin', 'pdim', 'plist');
-    loaded = true;
+    fprintf('\nSucceed to load data from\n\n%s\n\n', matfile)
     qlist = secup(options);
     fun = @(p) ismember(p, qlist);
     pind = find(cellfun(fun, plist));
@@ -27,23 +42,16 @@ if (isfield(options, 'reload') && options.reload == true)
     fmin = fmin(pind);
     %pdim = pdim(pind);
     plist = plist(pind);
-end
-if ~loaded
-    [frec, output] = testcu(solvers, options);
-    fmin = min(min(min(frec, [], 4), [], 3), [], 2);
+else
+    delete(matfile);
+    [frec, fmin, output] = testcu(solvers, options);
     pdim = output.pdim;
     plist = output.plist;
     save(matfile, 'frec', 'fmin', 'pdim', 'plist');
 end
 
-% `outdir` is the directory to contain the figures (.eps, .pdf) and problem list (problem.txt).
-outdir = fullfile(data_dir, strcat(stamp, '.', time));
-if ~exist(outdir, 'dir')
-    mkdir(outdir);
-end
-
 % Record the tested problems in `problems.txt`.
-fprob = fullfile(outdir, strcat(stamp, '.', time, '.', 'problems.txt'));
+fprob = fullfile(outdir, strcat(stamp, '.', feature_and_time, '.', 'problems.txt'));
 fid = fopen(fprob, 'w');
 if fid >= 3
     for ip = 1 : length(plist)
@@ -68,23 +76,35 @@ prof_options = struct();
 prof_options.solvers = solvers;
 prof_options.outdir = outdir;
 prof_options.stamp = stamp;
-prof_options.time = time;
+prof_options.time = feature_and_time;
 
 prec = (1:10);
 nprec = length(prec);
 tau = 10.^(-prec);
 prof_output = cell(1,nprec);
+format long
 for iprec = 1 : nprec
     prof_options.tau = tau(iprec);
     prof_output{iprec} = perfprof(frec, fmin, prof_options);
     %dataprof(frec, fmin, pdim, prof_options);
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Appearance of the plots.
+%fontsize = 12;
+linewidth = 1;
+bleu=[0.16, 0.4470, 0.7410];
+vert=[0, 0.6, 0];
+colors  = {bleu, 'k', 'b', 'r', vert, bleu, 'k', 'b', 'r', vert};
+%lines   = {'-', '-.', '--', ':', '-', '-.', '--', ':', '-', '-.'};
+lines   = {'-', '-', '-', '-', '-', '-', '-', '-', '-', '-'};
 hfig = figure("visible", false, 'DefaultAxesPosition', [0, 0, 1, 1]);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 for iprec = 1 : nprec
     subplot(1, nprec, iprec);
     for is = 1 : ns
-       plot(prof_output{iprec}.profile{is}(1,:), prof_output{iprec}.profile{is}(2,:));
+       plot(prof_output{iprec}.profile{is}(1,:), prof_output{iprec}.profile{is}(2,:), ...
+           lines{is}, 'Color', colors{is},  'Linewidth', linewidth);
        hold on;
     end
     xlabel(sprintf('%d', iprec));
@@ -97,13 +117,11 @@ for iprec = 1 : nprec
 end
 
 % The following appears only in the last subplot, but it is sufficient for our use.
-if isfield(options, 'stamp')
-    ylabel(options.stamp);
-end
+ylabel(strrep(test_feature, '_', '\\_'));
 legend(solvers,'Location', 'southeast','Orientation','vertical');
 
 % Save the figure as eps.
-figname = strcat(stamp, '.', 'summary', '.', time);
+figname = strcat(stamp, '.', 'summary', '.', feature_and_time);
 epsname = fullfile(outdir, strcat(figname,'.eps'));
 set(gcf,'position',[0, 0, 4600,460]);
 saveas(hfig, epsname, 'epsc2');
@@ -111,10 +129,11 @@ saveas(hfig, epsname, 'epsc2');
 % Try converting the eps to pdf.
 try
     system(['epstopdf ',epsname]);
-    summary = fullfile(outdir, strcat(figname,'.pdf'));
+    summary_file = fullfile(outdir, strcat(figname,'.pdf'));
 catch
-    summary = epsname;
+    summary_file = epsname;
 end
+fprintf('\nSummary for problem type %s with test feature %s:\n\n%s\n\n', options.type, test_feature, summary_file);
 
 
 % For convenience, save a copy of `problems.txt` and the figures in data_dir. They will be
