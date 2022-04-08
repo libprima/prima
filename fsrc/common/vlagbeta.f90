46870d06d196cd202427d09936e7f5a213a3a9b1
Zaikun <zaikunzhang@gmail.com>
2022-04-09 03:59:46 +0800

    220409.035946.HKT VERSION3 of VLAGBETA

--- fsrc/common/vlagbeta.f90
+++ fsrc/common/vlagbeta.f90
@@ -10,7 +10,7 @@ module vlagbeta_mod
 !
 ! Started: July 2020
 !
-! Last Modified: Saturday, April 09, 2022 AM03:55:58
+! Last Modified: Saturday, April 09, 2022 AM03:58:00
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -198,9 +198,9 @@ bw = matprod(bmat(:, 1:npt), wcheck)
 bd = matprod(bmat(:, npt + 1:npt + n), d)
 !bsum = sum(bd * d + bw * d + bw * d)  ! VERSION 1
 
-bsum = inprod(bd + TWO * bw, d)  ! VERSION 2
+!bsum = inprod(bd + TWO * bw, d)  ! VERSION 2
 
-!bw = matprod(bmat, [TWO*wcheck, d]) ; bsum = inprod(bw, d)  ! VERSION 3
+bw = matprod(bmat, [TWO * wcheck, d]); bsum = inprod(bw, d)  ! VERSION 3
 
 beta = dx**2 + dsq * (xoptsq + TWO * dx + HALF * dsq) - omega_inprod(idz_loc, zmat, wcheck, wcheck) - bsum
 
