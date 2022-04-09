551489abdefe9884f76bfd6640b1f9e544cb72dc
Zaikun <zaikunzhang@gmail.com>
2022-04-09 21:07:08 +0800

    220409.210708.HKT accept VERSION7 of VLAGBETA
    Former-commit-id: bf1da3c568be293244b454b2a3c28a8feb75ab96 [formerly 78275b1014e07e8e317b6cd32beeafdfa6ae84f4]
    Former-commit-id: 82d657e4b3e51c696249bd6cfb0021ed22643bee

--- fsrc/common/vlagbeta.f90
+++ fsrc/common/vlagbeta.f90
@@ -2,14 +2,14 @@ module vlagbeta_mod
 !--------------------------------------------------------------------------------------------------!
 ! This module contains a subroutine that calculates VLAG and BETA for a given step D. Both VLAG and
 ! BETA are critical for the updating procedure of H, which is detailed formula (4.11) of the NEWUOA
-! paper. See (4.12) for the definition of BETA, and VLAG is indeed Hw; (4.25)--(4.26) formulate the
+! paper. See (4.12) for the definition of BETA, and VLAG is indeed H*w; (4.25)--(4.26) formulate the
 ! actual calculating scheme of VLAG and BETA.
 !
 ! N.B.:
 ! 1. In languages like MATLAB/Python/Julia/R, CALVLAG and CALBETA should be implemented into one
-! single function, as they share most of the calculation. We separate them in Fortran because
-! Fortran functions can only have one output.
-! 2. Given any D and t in {1, ..., NPT}, VLAG(t) = e_t^T Hw = L_t(XBASE + XOPT + D), where L_t is
+! single function, as they share most of the calculation. We separate them in Fortran (at the
+! expense of repeating some calculation) because Fortran functions can only have one output.
+! 2. Given any D and t in {1, ..., NPT}, VLAG(t) = e_t^T*H*w = L_t(XBASE + XOPT + D), where L_t is
 ! the t-th Lagrange basis function corresponding to the interpolation set that defines H. See
 ! (6.3)--(6.4) of the NEWUOA paper for details. As a consequence, SUM(VLAG(1:NPT)) = 1 in theory.
 !
@@ -19,7 +19,7 @@ module vlagbeta_mod
 !
 ! Started: July 2020
 !
-! Last Modified: Saturday, April 09, 2022 PM05:24:50
+! Last Modified: Saturday, April 09, 2022 PM09:06:30
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -32,7 +32,7 @@ contains
 
 function calvlag(kopt, bmat, d, xpt, zmat, idz) result(vlag)
 !--------------------------------------------------------------------------------------------------!
-! This function calculates VLAG = Hw for a given step D. See (4.25) of the NEWUOA paper.
+! This function calculates VLAG = H*w for a given step D. See (4.25) of the NEWUOA paper.
 !--------------------------------------------------------------------------------------------------!
 ! List of local arrays (including function-output arrays; likely to be stored on the stack):
 ! REAL(RP) :: VLAG(NPT+N), WCHECK(NPT), XOPT(N)
@@ -57,16 +57,16 @@ real(RP), intent(in) :: zmat(:, :)  ! ZMAT(NPT, NPT - N - 1)
 integer(IK), intent(in), optional :: idz  ! Absent in BOBYQA, being equivalent to IDZ = 1
 
 ! Outputs
-real(RP) :: vlag(size(bmat, 2))    ! VLAG(NPT + N)
+real(RP) :: vlag(size(xpt, 1) + size(xpt, 2))    ! VLAG(NPT + N)
 
 ! Local variables
 character(len=*), parameter :: srname = 'CALVLAG'
 integer(IK) :: idz_loc
 integer(IK) :: n
 integer(IK) :: npt
+real(RP) :: tol  ! For debugging only
 real(RP) :: wcheck(size(zmat, 1))
 real(RP) :: xopt(size(xpt, 1))
-real(RP) :: tol  ! For debugging only
 
 ! Sizes
 n = int(size(xpt, 1), kind(n))
@@ -101,28 +101,23 @@ xopt = xpt(:, kopt)  ! Read XOPT.
 ! (4.24) of the NEWUOA paper, and also hat{w} in eq(6.5) of
 ! M. J. D. Powell, Least Frobenius norm updating of quadratic models that satisfy interpolation
 ! conditions. Math. Program., 100:183--215, 2004
-wcheck = matprod(d, xpt)
-wcheck = wcheck * (HALF * wcheck + matprod(xopt, xpt))
 ! Assume that the |D| ~ DELTA, |XPT| ~ |XOPT|, and DELTA < |XOPT|. Then WCHECK is in the order of
 ! DELTA*|XOPT|^3, which is can quickly become tiny.
+wcheck = matprod(d, xpt)
+wcheck = wcheck * (HALF * wcheck + matprod(xopt, xpt))
 
-!vlag(1:npt) = matprod(d, bmat(:, 1:npt))
-!vlag(1:npt) = vlag(1:npt) + omega_mul(idz_loc, zmat, wcheck)
-
+! The following two lines set VLAG to H*(w-v).
 vlag(1:npt) = omega_mul(idz_loc, zmat, wcheck) + matprod(d, bmat(:, 1:npt))
-
-!------------------------------------------------------!
 vlag(npt + 1:npt + n) = matprod(bmat, [wcheck, d])
+! The following line is equivalent to the above one, but handles WCHECK and D separately.
 !vlag(npt + 1:npt + n) = matprod(bmat(:, 1:npt), wcheck) + matprod(bmat(:, npt + 1:npt + n), d)
-!------------------------------------------------------!
 
+! The following line sets VLAG(KOPT) to the correct value.
 vlag(kopt) = vlag(kopt) + ONE
 
 !====================!
 !  Calculation ends  !
 !====================!
-
-write (16, *) abs(sum(vlag(1:npt)) - 1.0_RP)
 
 ! Postconditions
 if (DEBUGGING) then
@@ -140,15 +135,15 @@ function calbeta(kopt, bmat, d, xpt, zmat, idz) result(beta)
 !--------------------------------------------------------------------------------------------------!
 ! List of local arrays (including function-output arrays; likely to be stored on the stack):
 ! REAL(RP) :: BW(N), BD(N), WCHECK(NPT), XOPT(N)
-! Size of local arrays: REAL(RP)*(NPT+3*N)
+! Size of local arrays: REAL(RP)*(3*NPT+4*N)
 !--------------------------------------------------------------------------------------------------!
 
 ! Generic modules
-use, non_intrinsic :: consts_mod, only : RP, IK, TWO, HALF, DEBUGGING
+use, non_intrinsic :: consts_mod, only : RP, IK, HALF, DEBUGGING
 use, non_intrinsic :: debug_mod, only : assert
 use, non_intrinsic :: infnan_mod, only : is_finite
 use, non_intrinsic :: linalg_mod, only : inprod, matprod, issymmetric
-use, non_intrinsic :: powalg_mod, only : omega_inprod, omega_mul
+use, non_intrinsic :: powalg_mod, only : omega_mul
 
 implicit none
 
@@ -168,16 +163,11 @@ character(len=*), parameter :: srname = 'CALBETA'
 integer(IK) :: idz_loc
 integer(IK) :: n
 integer(IK) :: npt
-real(RP) :: bw(size(bmat, 1))
-real(RP) :: bd(size(bmat, 1))
-real(RP) :: bsum
-real(RP) :: dsq
-real(RP) :: dxopt
+real(RP) :: vlag(size(xpt, 1) + size(xpt, 2))
 real(RP) :: wcheck(size(zmat, 1))
-real(RP) :: xopt(size(xpt, 1))
-real(RP) :: xoptsq
+real(RP) :: wmv(size(xpt, 1) + size(xpt, 2))
 real(RP) :: x(size(xpt, 1))
-real(RP) :: wv(size(xpt, 1) + size(xpt, 2)), Hwv(size(xpt, 1) + size(xpt, 2)), wvHwv, beta1
+real(RP) :: xopt(size(xpt, 1))
 
 ! Sizes
 n = int(size(xpt, 1), kind(n))
@@ -208,51 +198,36 @@ end if
 
 xopt = xpt(:, kopt)  ! Read XOPT.
 
-dxopt = inprod(d, xopt)
-dsq = inprod(d, d)
-xoptsq = inprod(xopt, xopt)
-
 ! WCHECK contains the first NPT entries of (w-v) for the vectors w and v defined in (4.10) and
 ! (4.24) of the NEWUOA paper, and also hat{w} in eq(6.5) of
 ! M. J. D. Powell, Least Frobenius norm updating of quadratic models that satisfy interpolation
 ! conditions. Math. Program., 100:183--215, 2004
+! Assume that the |D| ~ DELTA, |XPT| ~ |XOPT|, and DELTA < |XOPT|. Then WCHECK is in the order of
+! DELTA*|XOPT|^3, which is can quickly become tiny.
 wcheck = matprod(d, xpt)
 wcheck = wcheck * (HALF * wcheck + matprod(xopt, xpt))
 
-!bw = matprod(bmat(:, 1:npt), wcheck)
-!bd = matprod(bmat(:, npt + 1:npt + n), d)
-!bsum = sum(bd * d + bw * d + bw * d)  ! VERSION 1
+! WMV is the vector (w-v) for the w and v defined in (4.10) and (4.24) of the NEWUOA paper.
+wmv = [wcheck, d]
+! The following two lines set VLAG to H*(w-v).
+vlag(1:npt) = omega_mul(idz_loc, zmat, wcheck) + matprod(d, bmat(:, 1:npt))
+vlag(npt + 1:npt + n) = matprod(bmat, wmv)
+! The following line is equivalent to the above one, but handles WCHECK and D separately.
+!vlag(npt + 1:npt + n) = matprod(bmat(:, 1:npt), wcheck) + matprod(bmat(:, npt + 1:npt + n), d)
 
-!bsum = inprod(bd + TWO * bw, d)  ! VERSION 2
-!bsum = inprod(bd + bw + bw, d)  ! VERSION 5
-!bsum = inprod(bw + bw + bd, d)  ! VERSION 5
+x = xopt + d
+beta = HALF * (inprod(x, x)**2 + inprod(xopt, xopt)**2) - inprod(x, xopt)**2 - inprod(vlag, wmv)
+! Powell's version of the above two lines is
+!----------------------------------------------------------------------------------!
+!!BETA = DXOPT**2 + DSQ * (XOPTSQ + DXOPT + DXOPT + HALF * DSQ) - INPROD(VLAG, WMV)
+!where, DXOPT = INPROD(D, XOPT), DSQ = INPROD(D, D), XOPTSQ = INPROD(XOPT, XOPT).
+!----------------------------------------------------------------------------------!
 
-!bw = matprod(bmat, [TWO * wcheck, d]); bsum = inprod(bw, d)  ! VERSION 3
+! N.B.: We can evaluate INPROD(VLAG, WMV) as INPROD(VLAG(1:NPT), WCHECK) + INPROD(VLAG(NPT+1:NPT+N),D)
+! if it is desirable to handle WCHECK and D separately due to their significantly different magnitudes.
 
-!beta = dxopt**2 + dsq * (xoptsq + TWO * dxopt + HALF * dsq) - omega_inprod(idz_loc, zmat, wcheck, wcheck) - bsum  ! VERSIONa
-!beta = dxopt**2 + dsq * (xoptsq + dxopt + dxopt + HALF * dsq) - omega_inprod(idz_loc, zmat, wcheck, wcheck) - bsum  ! VERSIONb
-
-
-!beta = inprod(wcheck, omega_mul(idz_loc, zmat, wcheck) + matprod(d, bmat(:, 1:npt))) + inprod(d, matprod(bmat, [wcheck, d]))
-!beta = inprod(wcheck, omega_mul(idz_loc, zmat, wcheck) + matprod(d, bmat(:, 1:npt))) + inprod(d, matprod(bmat, [wcheck, d]))
-
-
-!----------------------------------------------------------------------------------------!
-wv = [wcheck, d]
-Hwv = [omega_mul(idz_loc, zmat, wcheck) + matprod(d, bmat(:, 1:npt)), matprod(bmat, wv)]
-wvHwv = inprod(wv, Hwv)
-!----------------------------------------------------------------------------------------!
-
-!Hwv = [omega_mul(idz_loc, zmat, wcheck) + matprod(d, bmat(:, 1:npt)), &
-!    & matprod(bmat(:, 1:npt), wcheck) + matprod(bmat(:, npt + 1:npt + n), d)]
-!wvHwv = inprod(wcheck, Hwv(1:npt)) + inprod(d, Hwv(npt + 1:npt + n))
-
-!x = xopt + d
-!beta = HALF * (inprod(x, x)**2 + inprod(xopt, xopt)**2) - inprod(x, xopt)**2 - wvHwv
-
-beta = dxopt**2 + dsq * (xoptsq + dxopt + dxopt + HALF * dsq) - wvHwv
-
-!write (*, *) abs(beta1 - beta) / max(1.0_RP, abs(beta))
+! The following line sets VLAG(KOPT) to the correct value if we intend to output VLAG.
+!!vlag(kopt) = vlag(kopt) + ONE
 
 !====================!
 !  Calculation ends  !
@@ -260,4 +235,5 @@ beta = dxopt**2 + dsq * (xoptsq + dxopt + dxopt + HALF * dsq) - wvHwv
 
 end function calbeta
 
-end module vlagbeta_mod
+
+end vlagbeta_mod
