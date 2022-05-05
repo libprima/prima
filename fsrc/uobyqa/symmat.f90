2266beb0c0dc9d517f1b369e9a47954fdb021aa3
Zaikun <zaikunzhang@gmail.com>
2022-05-05 09:43:37 +0800

    220505.094337.HKT use `fplus` instead of `fval`

--- fsrc/uobyqa/symmat.f90
+++ fsrc/uobyqa/symmat.f90
@@ -9,12 +9,12 @@ module symmat_mod
 !
 ! Started: February 2022
 !
-! Last Modified: Wednesday, May 04, 2022 PM08:48:30
+! Last Modified: Thursday, May 05, 2022 AM08:05:10
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
 private
-public :: vec2smat, smat2vec, smat_mul_vec, smat_fnorm
+public :: vec2smat, smat2vec, smat_mul_vec
 
 
 contains
@@ -152,52 +152,6 @@ end do
 !====================!
 
 end function smat_mul_vec
-
-
-function smat_fnorm(smatv) result(fnorm)
-!--------------------------------------------------------------------------------------------------!
-! This function calculates the Frobenius norm of a symmetric matrix,  with the upper triangular part
-! of the matrix stored in the vector SMATV column by column.
-!--------------------------------------------------------------------------------------------------!
-use, non_intrinsic :: consts_mod, only : RP, IK, ZERO, TWO, DEBUGGING
-use, non_intrinsic :: debug_mod, only : assert
-implicit none
-! Inputs
-real(RP), intent(in) :: smatv(:)
-! Outputs
-real(RP) :: fnorm, temp
-! Local variables
-character(len=*), parameter :: srname = 'SMAT_FNORM'
-integer(IK) :: ih, i
-integer(IK) :: n
-integer(IK) :: j
-
-! Sizes
-n = int((floor(sqrt(real(8 * size(smatv) + 1))) - 1) / 2, IK)
-
-! Preconditions
-if (DEBUGGING) then
-    call assert(size(smatv) == n * (n + 1_IK) / 2_IK, 'SIZE(SMATV) = N*(N+1)/2', srname)
-end if
-
-!====================!
-! Calculation starts !
-!====================!
-
-fnorm = ZERO
-ih = 0
-do j = 1, n
-    ih = (j - 1_IK) * j / 2_IK
-    fnorm = fnorm + TWO * sum(smatv(ih + 1:ih + j - 1)**2) + smatv(ih + j)**2
-end do
-
-fnorm = sqrt(fnorm)
-
-!====================!
-! Calculation starts !
-!====================!
-
-end function smat_fnorm
 
 
 end module symmat_mod
