becdcd27bed0bf9cc08c151d57540b184023a46b
zaikunzhang <zaikunzhang@gmail.com>
2022-05-31 09:10:38 +0800

    220531.091038.HKT LINCOA/trustregion.f90: change the initialization of ICOUNT

--- fsrc/lincoa/trustregion.f90
+++ fsrc/lincoa/trustregion.f90
@@ -11,7 +11,7 @@ module trustregion_mod
 !
 ! Started: February 2022
 !
-! Last Modified: Tuesday, May 31, 2022 AM01:06:32
+! Last Modified: Tuesday, May 31, 2022 AM08:40:33
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -156,14 +156,14 @@ s = ZERO
 ss = ZERO
 reduct = ZERO
 ngetact = 0
-get_act = .true.  ! Better name? 
+get_act = .true.  ! Better name?
 
-icount = nact  ! Artificial value. Not used. To entertain compilers. To be removed.
+icount = -1_IK  ! Artificial value. Not used. To entertain compilers. To be removed.
 
 do while (.true.)  !TODO: prevent infinite cycling
     if (get_act) then
-        ! GETACT picks the active set for the current S. It also sets DW to the vector closest to -G 
-        ! that is orthogonal to the normals of the active constraints. DW is scaled to have length 
+        ! GETACT picks the active set for the current S. It also sets DW to the vector closest to -G
+        ! that is orthogonal to the normals of the active constraints. DW is scaled to have length
         ! 0.2*SNORM, as then a move of DW from S is allowed by the linear constraints.
         ngetact = ngetact + 1
         call getact(amat, g, snorm, iact, nact, qfac, resact, resnew, rfac, dw)
@@ -174,20 +174,20 @@ do while (.true.)  !TODO: prevent infinite cycling
         scaling = 0.2_RP * snorm / sqrt(dd)
         dw = scaling * dw
 
-        ! If the modulus of the residual of an active constraint is substantial, then set D to the 
+        ! If the modulus of the residual of an active constraint is substantial, then set D to the
         ! shortest move from S to the boundaries of the active constraints.
         bstep = ZERO
         if (any(resact(1:nact) > 1.0E-4_RP * snorm)) then
-            ! N.B.: We prefer `ANY(X > Y)` to `MAXVAL(X) > Y`, as Fortran standards do not specify 
-            ! MAXVAL(X) when X contains NaN, and MATLAB/Python/R/Julia behave differently in this 
-            ! respect. Moreover, MATLAB defines max(X) = [] if X == [], differing from mathematics 
+            ! N.B.: We prefer `ANY(X > Y)` to `MAXVAL(X) > Y`, as Fortran standards do not specify
+            ! MAXVAL(X) when X contains NaN, and MATLAB/Python/R/Julia behave differently in this
+            ! respect. Moreover, MATLAB defines max(X) = [] if X == [], differing from mathematics
             ! and other languages.
             vlam(1:nact) = solve(transpose(rfac(1:nact, 1:nact)), resact(1:nact))
             !!MATLAB: vlam(1:nact) = rfac(1:nact, 1:nact)' \ resact(1:nact)
             d = matprod(qfac(:, 1:nact), vlam(1:nact))
 
             ! The vector D that has just been calculated is also the shortest move from S+DW to the
-            ! boundaries of the active constraints. Set BSTEP to the greatest steplength of this 
+            ! boundaries of the active constraints. Set BSTEP to the greatest steplength of this
             ! move that satisfies the trust region bound.
             ds = inprod(d, s + dw)
             dd = sum(d**2)
@@ -212,7 +212,7 @@ do while (.true.)  !TODO: prevent infinite cycling
             bstep = minval([bstep, ONE, frac])  ! BSTEP = MINVAL([BSTEP, ONE, FRAC(TRUELOC(AD>0))])
         end if
 
-        ! Set the next direction for seeking a reduction in the model function subject to the trust 
+        ! Set the next direction for seeking a reduction in the model function subject to the trust
         ! region bound and the linear constraints.
         if (bstep > 0) then
             d = dw + bstep * d
@@ -243,7 +243,7 @@ do while (.true.)  !TODO: prevent infinite cycling
 
     dw = hess_mul(d, xpt, pq, hq)
 
-    ! Set DGD to the curvature of the model along D. Then reduce ALPHA if necessary to the value 
+    ! Set DGD to the curvature of the model along D. Then reduce ALPHA if necessary to the value
     ! that minimizes the model.
     dgd = inprod(d, dw)
     alpht = alpha
@@ -266,9 +266,9 @@ do while (.true.)  !TODO: prevent infinite cycling
     end if
     !----------------------------------------------------------------------------------------------!
     ! Alternatively, JSAV and ALPHA can be calculated as below.
-    !JSAV = INT(MINLOC([ALPHA, FRAC], DIM=1), IK) - 1_IK  
+    !JSAV = INT(MINLOC([ALPHA, FRAC], DIM=1), IK) - 1_IK
     !ALPHA = MINVAL([ALPHA, FRAC])  ! This line cannot be exchanged with the last.
-    ! We prefer our implementation as the code is more explicit; in addition, it is more flexible: 
+    ! We prefer our implementation as the code is more explicit; in addition, it is more flexible:
     ! we can change the condition ANY(FRAC < ALPHA) to ANY(FRAC < (1 - EPS) * ALPHA) or
     ! ANY(FRAC < (1 + EPS) * ALPHA), depending on whether we believe a false positive or a false
     ! negative of JSAV > 0 is more harmful.
@@ -289,10 +289,10 @@ do while (.true.)  !TODO: prevent infinite cycling
     end if
     reduct = reduct - alpha * (dg + HALF * alpha * dgd)
 
-    ! Test for termination. Branch to a new loop if there is a new active constraint and if the 
+    ! Test for termination. Branch to a new loop if there is a new active constraint and if the
     ! distance from S to the trust region boundary is at least 0.2*SNORM.
 
-    ! Zaikun 2019-08-29: the code can encounter infinite cycling due to NaN values. Exit when 
+    ! Zaikun 2019-08-29: the code can encounter infinite cycling due to NaN values. Exit when
     ! NGETACT is large or NaN is detected.
     ! Caution: 1. MIN accepts only data with the same KIND; 2. Integer overflow.
     if (ngetact > min(10000, 100 * int(m + 1) * int(n)) .or.  &
@@ -315,7 +315,7 @@ do while (.true.)  !TODO: prevent infinite cycling
     if (icount == n) exit
 
     ! Calculate the next search direction, which is conjugate to the previous one if ICOUNT /= NACT.
-    ! N.B.: NACT < 0 is impossible unless GETACT is buggy; NACT = 0 can happen, particularly if 
+    ! N.B.: NACT < 0 is impossible unless GETACT is buggy; NACT = 0 can happen, particularly if
     ! there is no constraint. In theory, the code for the second case below covers the first as well.
     if (nact <= 0) then
         gw = g
