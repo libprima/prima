ebd0639de4ea188fb0fd0e7d138ba3c7a80f08a9
zaikunzhang <zaikunzhang@gmail.com>
2022-08-20 04:50:52 +0800

    220820.045052.HKT block a problem for COBYLA

--- base/220227/matlab/tests/private/isequiv.m
+++ base/220227/matlab/tests/private/isequiv.m
@@ -116,7 +116,7 @@ if isempty(requirements.list)
         blacklist = [blacklist, {' QPNBOEI2', 'QPCBOEI2', 'EXTRASIM', 'SIM2BQP', 'BQP1VAR', 'LUKSAN22LS'}]; % Too long to solve
     case {'cobyla', 'cobylan'}
         blacklist = [blacklist, {'PALMER4ANE', 'PALMER5BNE'}];
-        blacklist = [blacklist, {'LSNNODOC', 'DEGENLPB', 'RES', 'AVION2', 'SIPOW3', 'HS55','PRODPL1'}];  % Takes long to solve
+        blacklist = [blacklist, {'LSNNODOC', 'DEGENLPB', 'RES', 'AVION2', 'SIPOW3', 'HS55','PRODPL1','BQPGASIM'}];  % Takes long to solve
         blacklist = [blacklist, {'MINMAXRB', 'MAKELA1'}]; % Classical COBYLA encounters SEGFAULT
         blacklist = [blacklist, {'POLAK6'}]; % Cannot pass  B = A^{-1}!
         if requirements.maxdim <= 50  % This means we intend to have a quick test with small problems
