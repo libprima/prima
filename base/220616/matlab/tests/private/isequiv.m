4bf3ec00af848abce63abe9f9039d49996f288d6
zaikunzhang <zaikunzhang@gmail.com>
2022-09-20 16:46:44 +0800

    block problems that take too long
    Former-commit-id: 294df33b6bf2d236a18cea8b68dae2ad24de26b5 [formerly e046e3fc73fb4848afe92e6f8211e3c530235b51]
    Former-commit-id: 031ab5f6c5a0b6afb6eba35bec71276080442888

--- base/220616/matlab/tests/private/isequiv.m
+++ base/220616/matlab/tests/private/isequiv.m
@@ -129,7 +129,7 @@ else
         blacklist = [blacklist, {'DUAL3', 'SPANHYD'}]; % Too long to solve
         blacklist = [blacklist, {'DUAL3', 'DUAL2', 'DUAL1','SIM2BQP', 'BQP1VAR', 'LUKSAN22LS','LUKSAN21LS'}]; % Too long to solve
     case {'cobyla', 'cobylan'}
-        blacklist = [blacklist, {'PALMER4ANE', 'PALMER5BNE'}];
+        blacklist = [blacklist, {'PALMER4ANE', 'PALMER5BNE', 'VESUVIA'}];
         blacklist = [blacklist, {'POLAK6', 'POLAK2'}]; % B = A^{-1} fails
         blacklist = [blacklist, {'MINMAXRB', 'MAKELA1'}]; % Classical COBYLA encounters SEGFAULT
         if requirements.maxdim <= 50  % This means we intend to have a quick test with small problems
