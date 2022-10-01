438e734a5782511e87a939a37a1f8635360c8b43
zaikunzhang <zaikunzhang@gmail.com>
2022-10-01 17:08:15 +0800

    221001.170815.CST BOBYQA: improve the condition for RESCUE

--- fsrc/bobyqa/bobyqb.f90
+++ fsrc/bobyqa/bobyqb.f90
@@ -10,7 +10,7 @@ module bobyqb_mod
 !
 ! Started: February 2022
 !
-! Last Modified: Saturday, October 01, 2022 PM03:59:29
+! Last Modified: Saturday, October 01, 2022 PM04:58:31
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -341,7 +341,7 @@ do while (.true.)
         !if (tr_success .and. .not. any(den > 0.5 * maxval(vlag(1:npt)**2))) then
         !if (.not. any(den > HALF * maxval(vlag(1:npt)**2))) then
         !if (.not. any(den > maxval(vlag(1:npt)**2))) then
-        if (tr_success .and. .not. any(den > maxval(vlag(1:npt)**2))) then
+        if (tr_success .and. .not. (is_finite(sum(abs(vlag))) .and. any(den > maxval(vlag(1:npt)**2)))) then
             if (nf == nfresc) then  ! This cannot happen.
                 info = DAMAGING_ROUNDING
                 exit
@@ -477,7 +477,6 @@ do while (.true.)
 
         ! If a trust region step has provided a sufficient decrease in F, then branch for another
         ! trust region calculation.
-        !if (f <= fopt - TENTH * qred) cycle
         improve_geo = .not. (knew > 0 .and. f <= fopt - TENTH * qred) !???
         ! Should we always take a trust region step after RESCUE?
         if (.not. improve_geo) then
@@ -513,12 +512,14 @@ do while (.true.)
 
             ! Calculate a geometry step.
             d = geostep(knew, kopt, bmat, delbar, sl, su, xpt, zmat)
+            xnew = min(max(sl, xopt + d), su)
 
             ! Call RESCUE if if rounding errors have damaged the denominator corresponding to D.
             ! It provides a useful safeguard, but is not invoked in most applications of BOBYQA.
             vlag = calvlag(kopt, bmat, d, xpt, zmat)
             den = calden(kopt, bmat, d, xpt, zmat)
-            if (den(knew) <= HALF * vlag(knew)**2 .or. is_nan(den(knew))) then
+            !if (.not. (is_finite(sum(abs(vlag))) .and. den(knew) > HALF * vlag(knew)**2)) then
+            if (.not. (is_finite(sum(abs(vlag))) .and. den(knew) > vlag(knew)**2)) then
                 if (nf == nfresc) then
                     info = DAMAGING_ROUNDING
                     exit
@@ -540,6 +541,7 @@ do while (.true.)
                 ! model ans [BMAT, ZMAT]; in this case, we calculate a new geometry step.
                 if (nfresc == nf) then
                     d = geostep(knew, kopt, bmat, delbar, sl, su, xpt, zmat)
+                    xnew = min(max(sl, xopt + d), su)
                 else
                     nfresc = nf
                     cycle
@@ -548,7 +550,7 @@ do while (.true.)
 
             ! Put the variables for the next calculation of the objective function in XNEW, with any
             ! adjustments for the bounds. In precise arithmetic, X = XBASE + XNEW.
-            xnew = min(max(sl, xopt + d), su)
+            !xnew = min(max(sl, xopt + d), su)
             x = min(max(xl, xbase + xnew), xu)
             x(trueloc(xnew <= sl)) = xl(trueloc(xnew <= sl))
             x(trueloc(xnew >= su)) = xu(trueloc(xnew >= su))
