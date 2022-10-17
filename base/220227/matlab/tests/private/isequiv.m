b59edcdfff17a2d038c20ab153a7cfbad2ba875e
zaikunzhang <zaikunzhang@gmail.com>
2022-10-17 08:39:17 +0800

    221017.083917.CST block testing problems
    Former-commit-id: 8fff2195c57f327f3084da4ffb6ddbb4f6f4050b [formerly 0fd7d7f3d4a81616e2f4ea1b3009fe81897233b9]
    Former-commit-id: f62d4dd1bb59e95bd4e79cbd763fda602efd2e93

--- base/220227/matlab/tests/private/isequiv.m
+++ base/220227/matlab/tests/private/isequiv.m
@@ -119,8 +119,8 @@ if isempty(requirements.list)
     case {'cobyla', 'cobylan'}
         blacklist = [blacklist, {'POLAK6', 'POLAK2'}]; % B = A^{-1} fails
         blacklist = [blacklist, {'PALMER4ANE', 'PALMER5BNE'}];
-        blacklist = [blacklist, {'LSNNODOC', 'DEGENLPB', 'RES', 'AVION2', 'SIPOW3', 'HS55','PRODPL1','BQPGASIM','LSNNODOC', 'VESUVIA', 'MESH', 'LOADBAL','MIFFLIN1'}];  % Takes long to solve
-        blacklist = [blacklist, {'MINMAXRB', 'MAKELA1', 'HS75'}]; % Classical COBYLA encounters SEGFAULT
+        blacklist = [blacklist, {'LSNNODOC', 'DEGENLPB', 'RES', 'AVION2', 'SIPOW3', 'HS55','PRODPL1','BQPGASIM','LSNNODOC', 'VESUVIA', 'MESH', 'LOADBAL','MIFFLIN1', 'ACOPR14'}];  % Takes long to solve
+        blacklist = [blacklist, {'MINMAXRB', 'MAKELA1', 'HS75', 'GAUSS3'}]; % Classical COBYLA encounters SEGFAULT
         blacklist = [blacklist, {'POLAK6'}]; % Cannot pass  B = A^{-1}!
         if requirements.maxdim <= 50  % This means we intend to have a quick test with small problems
             blacklist=[blacklist, {'BLEACHNG'}];  % A 17 dimensional bound-constrained problem that
--- base/220926/matlab/tests/private/isequiv.m
+++ base/220926/matlab/tests/private/isequiv.m
@@ -131,12 +131,12 @@ else
     case {'cobyla', 'cobylan'}
         blacklist = [blacklist, {'PALMER4ANE', 'PALMER5BNE','MESH', 'LOADBAL','MIFFLIN1'}];
         blacklist = [blacklist, {'POLAK6', 'POLAK2'}]; % B = A^{-1} fails
-        blacklist = [blacklist, {'MINMAXRB', 'MAKELA1', 'HS75'}]; % Classical COBYLA encounters SEGFAULT
+        blacklist = [blacklist, {'MINMAXRB', 'MAKELA1', 'HS75', 'GAUSS3'}]; % Classical COBYLA encounters SEGFAULT
         if requirements.maxdim <= 50  % This means we intend to have a quick test with small problems
             blacklist=[blacklist, {'BLEACHNG'}];  % A 17 dimensional bound-constrained problem that
                                                   % takes too much time for a small problem
         end
-        blacklist = [blacklist, {'PRODPL0', 'DEGENLPB', 'ANTWERP', 'SIPOW3', 'AVION2', 'HIMMELBI', 'HS55','PRODPL1', 'BQPGASIM', 'POLAK2'}]; % Takes long to solve
+        blacklist = [blacklist, {'PRODPL0', 'DEGENLPB', 'ANTWERP', 'SIPOW3', 'AVION2', 'HIMMELBI', 'HS55','PRODPL1', 'BQPGASIM', 'POLAK2', 'ACOPR14'}]; % Takes long to solve
         blacklist=[blacklist, {'DMN15102', 'DMN15103', 'DMN15332', 'DMN15333', 'DMN37142', 'DMN37143'}]; % Takes more than 5 min to solve
         blacklist = [blacklist, {'KISSING2', 'LUKSAN16', 'QPCBLEND', 'VANDERM4'}]; % Takes more than 20 sec to solve
         %blacklist = [blacklist, {'DUAL2', 'FEEDLOC', 'GROUPING', 'HYDCAR20', 'LINSPANH', 'LUKSAN11', ...
