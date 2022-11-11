9c69f68da6b9b7aaf0295a42f269bac7baba679c
zaikunzhang <zaikunzhang@gmail.com>
2022-11-11 09:26:12 +0800

    221111.092612.CST COBYLA: initialize PREREF/C/M; revise the definition of BAD_TRSTP using RATIO
    Former-commit-id: 116134b2fb80b8be86f24327ff778b9081279f7a [formerly 40d80cdfccaaa8c4a8be589d8f74ad5076a30913]
    Former-commit-id: 83138c76d8ec4f58a6b7110a577d02a760a74714

--- fsrc/uobyqa/uobyqb.f90
+++ fsrc/uobyqa/uobyqb.f90
@@ -11,7 +11,7 @@ module uobyqb_mod
 !
 ! Started: February 2022
 !
-! Last Modified: Thursday, November 10, 2022 PM11:25:57
+! Last Modified: Friday, November 11, 2022 AM09:02:15
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -375,9 +375,11 @@ do while (.true.)
     !improve_geo = bad_trstep .and. .not. (shortd .and. accurate_mod) .and. .not. close_itpset
     !reduce_rho = bad_trstep .and. (dnorm <= rho) .and. (.not. improve_geo)
 
-    bad_trstep = (shortd .or. (.not. qred > 0) .or. (ratio <= TENTH .and. ddmove <= 4.0_RP * rho**2) .or. knew_tr == 0)  ! For IMPROVE_GEO
+    ! BAD_TRSTEP for IMPROVE_GEO.
+    bad_trstep = (shortd .or. (.not. qred > 0) .or. (ratio <= TENTH .and. ddmove <= 4.0_RP * rho**2) .or. knew_tr == 0)
     improve_geo = bad_trstep .and. .not. adequate_mod
-    bad_trstep = (shortd .or. (.not. qred > 0) .or. (ratio <= 0 .and. ddmove <= 4.0_RP * rho**2) .or. knew_tr == 0)  ! For REDUCE_RHO
+    ! BAD_TRSTEP for REDUCE_RHO.
+    bad_trstep = (shortd .or. (.not. qred > 0) .or. (ratio <= 0 .and. ddmove <= 4.0_RP * rho**2) .or. knew_tr == 0)
     reduce_rho = bad_trstep .and. adequate_mod .and. small_trrad
 
 
