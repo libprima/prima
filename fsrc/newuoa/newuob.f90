8fef0c52c0753b3b16972f9fea6a145b15a64524
zaikunzhang <zaikunzhang@gmail.com>
2022-11-12 21:33:18 +0800

    221112.213318.CST NEWUOA: verify another definition of IMPROVE_GEO
    Former-commit-id: 6835f3020264b37179af17ff21abf297e955c96e [formerly 1b4bcfa1f1bd5537d7428f2353dbc90f5ee77e93]
    Former-commit-id: 4f0bc2cd515f2fccbb9b7103d3bb665961595272

--- fsrc/newuoa/newuob.f90
+++ fsrc/newuoa/newuob.f90
@@ -8,7 +8,7 @@ module newuob_mod
 !
 ! Started: July 2020
 !
-! Last Modified: Saturday, November 12, 2022 PM09:30:28
+! Last Modified: Saturday, November 12, 2022 PM09:32:26
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -359,11 +359,11 @@ do tr = 1, maxtr
     bad_trstep = (shortd .or. (.not. qred > 0) .or. ratio <= 0 .or. knew_tr == 0)
     reduce_rho = bad_trstep .and. adequate_geo .and. small_trrad
     ! Equivalently, REDUCE_RHO can be set as follows. It shows that REDUCE_RHO is TRUE in two cases.
-    reduce_rho = (shortd .and. accurate_mod) .or. (bad_trstep .and. close_itpset .and. small_trrad)
+    !reduce_rho = (shortd .and. accurate_mod) .or. (bad_trstep .and. close_itpset .and. small_trrad)
 
     ! With REDUCE_RHO properly defined, we can also set IMPROVE_GEO as follows.
-    !bad_trstep = (shortd .or. (.not. qred > 0) .or. ratio <= TENTH .or. knew_tr == 0)
-    !improve_geo = bad_trstep .and. (.not. reduce_rho) .and. (.not. close_itpset)
+    bad_trstep = (shortd .or. (.not. qred > 0) .or. ratio <= TENTH .or. knew_tr == 0)
+    improve_geo = bad_trstep .and. (.not. reduce_rho) .and. (.not. close_itpset)
     ! With IMPROVE_GEO properly defined, we can also set REDUCE_RHO as follows.
     !bad_trstep = (shortd .or. (.not. qred > 0) .or. ratio <= 0 .or. knew_tr == 0)
     !reduce_rho = bad_trstep .and. (.not. improve_geo) .and. small_trrad
