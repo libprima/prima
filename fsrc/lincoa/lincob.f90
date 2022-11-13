78a1adfa5a2ccae9ef8c5a3d1bee793e8c73c242
zaikunzhang <zaikunzhang@gmail.com>
2022-11-13 15:39:19 +0800

    221113.153919.CST LINCOA: try the alternative defitions of IMPROVE_GEO/REDUCE_RHO together
    Former-commit-id: 51491e4d5a8bf320ba70afbb89111e1ebce8a035 [formerly ff000fccd90ca8597463abae1d3006ebdb7a14ee]
    Former-commit-id: e5310f20b861d49d22dff4dc63e478399fb35913

--- fsrc/lincoa/lincob.f90
+++ fsrc/lincoa/lincob.f90
@@ -17,7 +17,7 @@ module lincob_mod
 !
 ! Started: February 2022
 !
-! Last Modified: Sunday, November 13, 2022 PM03:37:38
+! Last Modified: Sunday, November 13, 2022 PM03:39:00
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -488,15 +488,15 @@ do while (.true.)
     reduce_rho = bad_trstep .and. adequate_geo .and. small_trrad
 
     ! Equivalently, REDUCE_RHO can be set as follows. It shows that REDUCE_RHO is TRUE in two cases.
-    ! !reduce_rho = (shortd .and. accurate_mod) .or. (bad_trstep .and. close_itpset .and. small_trrad)
+    reduce_rho = (shortd .and. accurate_mod) .or. (bad_trstep .and. close_itpset .and. small_trrad)
 
     ! With REDUCE_RHO properly defined, we can also set IMPROVE_GEO as follows.
     bad_trstep = (shortd .or. (.not. qred > 0) .or. ratio <= TENTH .or. knew_tr == 0)
     improve_geo = bad_trstep .and. (.not. reduce_rho) .and. (.not. close_itpset)
 
     ! With IMPROVE_GEO properly defined, we can also set REDUCE_RHO as follows.
-    ! !bad_trstep = (shortd .or. (.not. qred > 0) .or. ratio <= 0 .or. knew_tr == 0)
-    ! !reduce_rho = bad_trstep .and. (.not. improve_geo) .and. small_trrad
+    bad_trstep = (shortd .or. (.not. qred > 0) .or. ratio <= 0 .or. knew_tr == 0)
+    reduce_rho = bad_trstep .and. (.not. improve_geo) .and. small_trrad
 
     ! LINCOA never sets IMPROVE_GEO and REDUCE_RHO to TRUE simultaneously.
     !call assert(.not. (improve_geo .and. reduce_rho), 'IMPROVE_GEO or REDUCE_RHO is false', srname)
