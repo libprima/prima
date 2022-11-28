c698aae5e78745aae4d3b051194172ff62e9dd73
zaikunzhang <zaikunzhang@gmail.com>
2022-11-29 00:09:41 +0800

    221129.000941.CST generate a new base!
    Former-commit-id: 3f2a1153e25d88952ab677aa4c8c4c063c77bb78 [formerly 542959fc18aec0f1f6c5d363f95441199bb38dc0]
    Former-commit-id: 8c1ca61933c203cb0d1eee58e6d9480b467e15ac

--- OPDFO/matlab/interfaces/private/postpdfo.m
+++ OPDFO/matlab/interfaces/private/postpdfo.m
@@ -563,9 +563,7 @@ if options.debug && ~options.classical
         fhistf = fhistf(chist <= max(cstrv_returned, 0));
     end
     minf = min([fhistf, fx]);
-%%%% Zaikun 2021-05-26: The following test is disabled for lincoa for the moment. lincoa may not pass it.
-%%%%    if (fx ~= minf) && ~(isnan(fx) && isnan(minf)) && ~(strcmp(solver, 'lincoa') && constr_modified)
-    if (fx ~= minf) && ~(isnan(fx) && isnan(minf)) && ~strcmp(solver, 'lincoa')
+    if (fx ~= minf) && ~(isnan(fx) && isnan(minf)) && ~(strcmp(solver, 'lincoa') && constr_modified)
         % Public/unexpected error
         error(sprintf('%s:InvalidFhist', invoker), ...
              '%s: UNEXPECTED ERROR: %s returns an fhist that does not match nf or fx.', invoker, solver);
