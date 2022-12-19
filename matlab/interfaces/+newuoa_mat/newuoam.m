5e78ce49ff98e8d79f1239174d5980b58001b923
zaikunzhang <zaikunzhang@gmail.com>
2022-12-19 23:25:47 +0800

    221219.232547.CST remove unnecessary comments in `newuoam.m`
    Former-commit-id: bbd856f2e52eb3962916ab06799b5defc92d02aa [formerly 37cbc5198209883ade13f6b614925dcc47ff4e62]
    Former-commit-id: 73670d8c0c15ee65879e8730528a8c5e1fbe98ed

--- matlab/interfaces/+newuoa_mat/newuoam.m
+++ matlab/interfaces/+newuoa_mat/newuoam.m
@@ -17,34 +17,28 @@
 
 function [x, f, exitflag, nf, xhist, fhist] = newuoam(calfun, x, rhobeg, rhoend, eta1, eta2, gamma1, gamma2, ftarget, maxfun, npt, iprint, maxhist, output_xhist, debugflag)
 	% A detailed introduction to the arguments is as follows.
-	% N.B.: RP and IK are defined in the module CONSTS_MOD. See consts.F90 under
-	% the directory name "common". By default, RP = kind(0.0D0) and IK = kind(0).
-	% Therefore, REAL(RP) is the double-precision real, and INTEGER(IK) is the
-	% default integer. For ADVANCED USERS, RP and IK can be defined by specifying
-	% __REAL_PRECISION__ and __INTEGER_KIND__ in common/ppf.h. Use the default if
-	% you are unsure.
-	%
+    %
 	% CALFUN
-	%   Input, subroutine.
-	%   CALFUN(X, F) should evaluate the objective function at the given
-	%   REAL(RP) vector X and set the value to the REAL(RP) scalar F. It
+	%   Input, function handle.
+	%   CALFUN(X) should evaluate the objective function at the given
+	%   vector X and set the value to the scalar F. It
 	%
 	% X
-	%   Input and outout, REAL(RP) vector.
+	%   Input and output, vector.
 	%   As an input, X should be an N dimensional vector that contains the
 	%   initial values of the variables, N being the dimension of the problem.
 	%   As an output, X will be set to an approximate minimizer.
 	%
 	% F
-	%   Output, REAL(RP) scalar.
+	%   Output, scalar.
 	%   F will be set to the objective function value of the X at exit.
 	%
 	% NF
-	%   Output, INTEGER(IK) scalar.
+	%   Output, scalar.
 	%   NF will be set to the number of function evaluations at exit.
 	%
 	% RHOBEG, RHOEND
-	%   Inputs, REAL(RP) scalars, default: RHOBEG = 1, RHOEND = 10^-6.
+	%   Inputs, scalars, default: RHOBEG = 1, RHOEND = 10^-6.
 	%   RHOBEG and RHOEND must be set to the initial and final values of a
 	%   trust region radius, so both must be positive with RHOEND <= RHOBEG.
 	%   Typically RHOBEG should be about one tenth of the greatest expected
@@ -52,28 +46,28 @@ function [x, f, exitflag, nf, xhist, fhist] = newuoam(calfun, x, rhobeg, rhoend,
 	%   required in the final values of the variables.
 	%
 	% FTARGET
-	%   Input, REAL(RP) scalar, default: - Infinity.
+	%   Input, scalar, default: - Infinity.
 	%   FTARGET is the target function value. The algorithm will terminate
 	%   when a point withi a function value <= FTARGET is found.
 	%
 	% MAXFUN
-	%   Input, INTEGER(IK) scalar, default: consts(maxfun_dim_dft)*N with
+	%   Input, scalar, default: consts(maxfun_dim_dft)*N with
 	%   consts(maxfun_dim_dft) defined in the module CONSTS_MOD (see consts.F90 in
 	%   the directory named "common").
 	%   MAXFUN is the maximal number of function evaluations.
 	%
 	% NPT
-	%   Input, INTEGER(IK) scalar, default: 2N + 1.
+	%   Input, scalar, default: 2N + 1.
 	%   NPT is the number of interpolation conditions for each trust region
 	%   model. Its value must be in the interval [N+2, (N+1)(N+2)/2].
 	%
 	% IPRINT
-	%   Input, INTEGER(IK) scalar, default: 0.
+	%   Input, scalar, default: 0.
 	%   The value of IPRINT should be set to 0, 1, -1, 2, -2, 3, or -3, which
 	%   controls how much information will be printed during the computation:
 	%   0: there will be no printing;
 	%   1: a message will be printed to the screen at the return, showing the
-	%      best vector of veriables found and its objective function value;
+	%      best vector of variables found and its objective function value;
 	%   2: in addition to 1, each new value of RHO is printed to the screen,
 	%      with the best vector of variables so far and its objective function
 	%      value;
@@ -86,7 +80,7 @@ function [x, f, exitflag, nf, xhist, fhist] = newuoam(calfun, x, rhobeg, rhoend,
 	%     be costly in terms of time and space.
 	%
 	% ETA1, ETA2, GAMMA1, GAMMA2
-	%   Input, REAL(RP) scalars, default: ETA1 = 0.1, ETA2 = 0.7, GAMMA1 = 0.5,
+	%   Input, scalars, default: ETA1 = 0.1, ETA2 = 0.7, GAMMA1 = 0.5,
 	%   and GAMMA2 = 2.
 	%   ETA1, ETA2, GAMMA1, and GAMMA2 are parameters in the updating scheme
 	%   of the trust region radius as detailed in the subroutine TRRAD in
@@ -97,17 +91,15 @@ function [x, f, exitflag, nf, xhist, fhist] = newuoam(calfun, x, rhobeg, rhoend,
 	%   Normally, ETA1 <= 0.25. It is NOT recommended to set ETA1 >= 0.5.
 	%
 	% XHIST, FHIST, MAXHIST
-	%   XHIST: Output, ALLOCATABLE rank 2 REAL(RP) array;
-	%   FHIST: Output, ALLOCATABLE rank 1 REAL(RP) array;
-	%   MAXHIST: Input, INTEGER(IK) scalar, default: MAXFUN
-	%   XHIST, if present, will output the history of iterates, while FHIST,
-	%   if present, will output the history function values. MAXHIST should
-	%   be a nonnegative integer, and XHIST/FHIST will output only the last
-	%   MAXHIST iterates and/or the corresponding function values. Therefore,
-	%   MAXHIST = 0 means XHIST/FHIST will output nothing, while setting
-	%   MAXHIST = MAXFUN ensures that  XHIST/FHIST will output all the history.
-	%   If XHIST is present, its size at exit will be (N, min(NF, MAXHIST));
-	%   if FHIST is present, its size at exit will be min(NF, MAXHIST).
+	%   XHIST: Output, rank 2 array;
+	%   FHIST: Output, rank 1 array;
+	%   MAXHIST: Input, scalar, default: MAXFUN
+	%   XHIST will output the history of iterates, while FHIST will output the history function
+    %   values. MAXHIST should be a nonnegative integer, and XHIST/FHIST will output only the last
+    %   MAXHIST iterates and/or the corresponding function values. Therefore, MAXHIST = 0 means
+    %   XHIST/FHIST will output nothing, while setting MAXHIST = MAXFUN ensures that  XHIST/FHIST
+    %   will output all the history. The size of XHIST at exit will be (N, min(NF,  MAXHIST));
+    %   the size of FHIST at exit will be min(NF, MAXHIST).
 	%
 	%   Important Notice:
 	%   Setting MAXHIST to a large value can be costly in terms of memory.
@@ -118,7 +110,7 @@ function [x, f, exitflag, nf, xhist, fhist] = newuoam(calfun, x, rhobeg, rhoend,
 	%   with caution%%%
 	%
 	% INFO
-	%   Output, INTEGER(IK) scalar.
+	%   Output, scalar.
 	%   INFO is the exit flag. It can be set to the following values defined
 	%   in the module INFO_MOD (see info.F90 under the directory named "common"):
 	%   infos(small_tr_radius): the lower bound for the trust region radius is reached;
@@ -128,14 +120,6 @@ function [x, f, exitflag, nf, xhist, fhist] = newuoam(calfun, x, rhobeg, rhoend,
 	%   infos(nan_x): NaN occurs in x;
 	%   infos(nan_inf_f): the objective function returns NaN or nearly infinite value;
 	%   infos(nan_model): NaN occurs in the models.
-
-	% Solver-specific modules
-
-	% Dummy variables
-
-	% Local variables
-
-	% Get size.
 
     % Replace any NaN or Inf in X by 0.
     x(isnan(x) | isinf(x)) = 0;
@@ -150,26 +134,6 @@ function [x, f, exitflag, nf, xhist, fhist] = newuoam(calfun, x, rhobeg, rhoend,
 	[x, nf, f, fhist, xhist, exitflag] = newuob(calfun, iprint, maxfun, npt, eta1, eta2, ftarget, gamma1, gamma2, rhobeg, rhoend, x, maxfhist, maxxhist, debugflag);
 	%--------------------------------------------------------------------------------------------------%
 
-	% Write the outputs.
-
-	% Copy XHIST to XHIST if needed.
-    % The SAFEALLOC line is removable in F2003.
     xhist = xhist(:, 1:min(nf, maxxhist));
-    % N.B.:
-    % 0. Allocate XHIST as long as it is present, even if MAXXHIST = 0;
-    % otherwise, it will be illeagle to enquire XHIST after exit.
-    % 1. Even though Fortran 2003 supports automatic (re)allocation of
-    % allocatable arrays upon intrinsic assignment, we keep the line of
-    % are still not standard-compliant in this respect.
-    % 3. When MAXXHIST > NF, which is the normal case in practice,
-    % XHIST contains GARBAGE in XHIST(:, NF + 1 : MAXXHIST).
-    % Therefore, we MUST cap XHIST at min(NF, MAXXHIST) so that XHIST
-    % cointains only valid history. For this reason, there is no way to
-    % avoid allocating two copies of memory for XHIST unless we declare
-    % it to be a POINTER instead of ALLOCATABLE.
-
-    % The SAFEALLOC line is removable in F2003.
     fhist = fhist(1:min(nf, maxfhist));
-    % The same as XHIST, we must cap FHIST at min(NF, MAXFHIST).
-
 end
