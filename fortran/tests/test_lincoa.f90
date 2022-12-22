82e04e30d622621524c6074f5535fc17fda7ac9d
zaikunzhang <zaikunzhang@gmail.com>
2022-12-22 14:54:51 +0800

    221222.145451.CST revise Fortran big tests
    Former-commit-id: 335c11f82b62ec3f30dd77e06f4fd846642adcb9 [formerly f6cc3966ea094d7c965b6cc2df14f9430c7cc5ec]
    Former-commit-id: 91e389c47f7be7163cd94f91963114ddbe176c53

--- fortran/tests/test_lincoa.f90
+++ fortran/tests/test_lincoa.f90
@@ -6,7 +6,7 @@ module test_solver_mod
 !
 ! Started: September 2021
 !
-! Last Modified: Thursday, December 22, 2022 AM11:29:45
+! Last Modified: Thursday, December 22, 2022 PM02:32:52
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -135,10 +135,10 @@ if (test_bigprob) then
         call setseed(rseed)
         m = int(min(int(10.0_RP * rand() * real(n, RP)), 10**floor(0.9 * real(min(range(0), range(0_IK))))), IK)
         call construct(prob, probname, n, m)
-        npt = int(TEN * rand() * real(n, RP), kind(npt))
-        iprint = 2
+        npt = max(n + 2_IK, int(5.0 * rand() * real(n, RP), kind(npt)))
+        iprint = 3
         maxfun = int(minval([10**min(range(0), range(0_IK)), int(npt) + int(500.0_RP * rand())]), IK)
-        maxhist = int(TWO * rand() * real(maxfun, RP), kind(maxhist))
+        maxhist = maxfun
         maxfilt = int(TWO * rand() * real(maxfun, RP), kind(maxfilt))
         if (rand() <= 0.5) then
             ctol = TEN**(-abs(5.0 * randn()))
