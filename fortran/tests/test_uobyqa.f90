a17953a8012456697ba5ffe8c73e8587e89fe531
zaikunzhang <zaikunzhang@gmail.com>
2022-12-24 19:39:28 +0800

    221224.193928.CST reschedule the tests
    Former-commit-id: 835b26a3004d384f699d883bc9d854b46c53d472 [formerly f4dcac5f28854fe50f3cc1842406e7e742eb3f2e]
    Former-commit-id: 64cd99f45b51c5a934f2dc55d4bd0782769ae261

--- fortran/tests/test_uobyqa.f90
+++ fortran/tests/test_uobyqa.f90
@@ -6,7 +6,7 @@ module test_solver_mod
 !
 ! Started: September 2021
 !
-! Last Modified: Saturday, December 24, 2022 AM12:54:59
+! Last Modified: Saturday, December 24, 2022 PM12:40:48
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -42,7 +42,7 @@ character(len=PNLEN) :: probname
 character(len=PNLEN) :: probs_loc(100)
 integer :: randseed_loc
 integer :: rseed
-integer(IK), parameter :: bign = 120_IK
+integer(IK), parameter :: bign = 200_IK
 integer(IK) :: dimstride_loc
 integer(IK) :: iprint
 integer(IK) :: iprob
@@ -116,7 +116,7 @@ if (test_bigprob) then
         npt = (n + 2_IK) * (n + 1_IK) / 2_IK
         maxfun = int(minval([10**min(range(0), range(0_IK)), int(npt) + int(500.0_RP * rand())]), IK)
         maxhist = maxfun
-        ftarget = -TEN**(min(10, range(0.0_RP)) * rand())
+        ftarget = -HUGENUM
         rhobeg = noisy(prob % Delta0)
         rhoend = max(1.0E-6_RP, rhobeg * 1.0E1_RP**(4.0_RP * rand() - 3.5_RP))
         call safealloc(x, n) ! Not all compilers support automatic allocation yet, e.g., Absoft.
