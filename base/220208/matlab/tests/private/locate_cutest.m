9b795fdf78b300e96507958083cea1dbb7d734f9
zaikunzhang <zaikunzhang@gmail.com>
2022-12-29 17:26:04 +0800

    221229.172604.CST try the new repo of cutest
    Former-commit-id: 9c13261b35dff6d76e96905bffb094eed822476c [formerly 8be07974a67c266659fb771d5fbc62d8f51fa7b8]
    Former-commit-id: d6097dd1962236a60a0c0859f784d5bc84256b83

--- base/220208/matlab/tests/private/locate_cutest.m
+++ base/220208/matlab/tests/private/locate_cutest.m
@@ -1,6 +1,6 @@
 function cpath = locate_cutest()
 %This function tells MATLAB where to find CUTEst. The following lines should be written according to
 % the installation of CUTEst on this machine.
-setenv('CUTEST', '~/local/cutesif/cutest');
-cpath = '~/local/cutesif/mtools/msrc';
+setenv('CUTEST', '~/local/matcutest/cutest');
+cpath = '~/local/matcutest/mtools/msrc';
 addpath(cpath);
