c3244fb8be690d0cb22ebea5514bd2761690dd19
zaikunzhang <zaikunzhang@gmail.com>
2023-01-08 10:19:07 +0800

    230108.101907.CST block problems that make the GitHub Actions overtime
    Former-commit-id: 5d8b8a713e3103d33ec2ed7ae697397982192b9d [formerly 667a0f0932b98251662ed5e158984de6726542ef]
    Former-commit-id: 68efcbd61909402fb842c194b2847626a532b643

--- base/220616/matlab/tests/private/isequiv.m
+++ base/220616/matlab/tests/private/isequiv.m
@@ -126,7 +126,7 @@ else
         blacklist = [blacklist, {'AVGASA', 'AVGASB'}];  % SEGFAULT on 20220306
         blacklist = [blacklist, {'CHEBYQAD'}]; % The classical lincoa encounters segfault
         blacklist = [blacklist, {'ARGTRIGLS', 'BROWNAL', 'PENALTY3', 'VARDIM'}]; % More than 10 minutes to solve.
-        blacklist = [blacklist, {'QPNBOEI2', 'QPCBOEI2', 'DECONVB'}]; % Too long to solve
+        blacklist = [blacklist, {'QPNBOEI2', 'QPCBOEI2', 'DECONVB', 'DECONVU'}]; % Too long to solve
         blacklist = [blacklist, {'DUAL3', 'SPANHYD'}]; % Too long to solve
         blacklist = [blacklist, {'DUAL3', 'DUAL2', 'DUAL1','SIM2BQP', 'BQP1VAR', 'LUKSAN22LS','LUKSAN21LS','MINSURF', 'QPCBLEND'}]; % Too long to solve
     case {'cobyla', 'cobylan'}
