82b7fa698bb69bde30c0158565ee6a82aae3afbf
zaikunzhang <zaikunzhang@gmail.com>
2023-01-11 13:13:29 +0800

    230111.131329.CST use try_mex_setup
    Former-commit-id: 7e0611c1ed4314245872bb4e5550d66a69a3b4b6 [formerly 3d92be1b3b1f43fc09d2bb861ed57a36e35b35d6]
    Former-commit-id: b9b42d4ad27dcdb618dd95e8c14935c7c56ee996

--- .github/scripts/intelvars_macos.sh
+++ .github/scripts/intelvars_macos.sh
@@ -1,7 +1,8 @@
 source /opt/intel/oneapi/setvars.sh
 sudo mkdir -p /usr/local/bin
-sudo ln -s $(which ifort) /usr/local/bin
-sudo ln -s $(which ifx) /usr/local/bin
-echo "ONEAPI_ROOT=/opt/intel/oneapi" >> "$GITHUB_ENV"
-echo "IFORT_COMPILER21=$(dirname $(dirname $(dirname $(which ifort))))" >> "$GITHUB_ENV"
-echo "PATH=$PATH:$(dirname $(which ifort)):$(dirname $(which ifx))"
+sudo ln -s "$(command -v ifort)" /usr/local/bin
+{
+echo ONEAPI_ROOT="/opt/intel/oneapi"
+echo IFORT_COMPILER21="$(dirname "$(dirname "$(dirname "$(command -v ifort)")")")"
+echo PATH='$PATH':"$(dirname "$(command -v ifort)")"
+} >> "$GITHUB_ENV"
