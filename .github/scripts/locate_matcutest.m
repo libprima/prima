e59d941ed8c5906611096e3db59bd3723c55c566
zaikunzhang <zaikunzhang@gmail.com>
2023-01-13 00:37:16 +0800

    230113.003716.CST use $GITHUB_ENV instead of setenv.sh

--- .github/scripts/locate_matcutest.m
+++ .github/scripts/locate_matcutest.m
@@ -9,6 +9,11 @@ if nargin < 1
 end
 
 [~, cmtools] = system(['find ', directory, ' -maxdepth 6 -wholename "*/matcutest/mtools/src" -type d -print -quit']);
+
+if isempty(cmtools)
+    error('locate_matcutest:MatCUTEstNotFound', 'MatCUTEst is not found under %s.', directory);
+end
+
 cmtools = strtrim(cmtools);  % Remove the leading and trailing white-space characters, including '\n'.
 
 cmpaths = {cmtools};  % There may be other paths to include in the future.
