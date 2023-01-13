d1d48293e5eb5556c5900db41c3e8d49a62a2a74
zaikunzhang <zaikunzhang@gmail.com>
2023-01-13 11:27:52 +0800

    230113.112752.CST try converting .github/acrtipts as a submodule

--- matlab/interfaces/lincoa.m
+++ matlab/interfaces/lincoa.m
@@ -201,14 +201,7 @@ function [x, fx, exitflag, output] = lincoa(varargin)
 %
 %   See https://www.libprima.net for more information.
 %
-%   ***********************************************************************
-%   Authors:    Tom M. RAGONNEAU (tom.ragonneau@connect.polyu.hk)
-%               and Zaikun ZHANG (zaikun.zhang@polyu.edu.hk)
-%               Department of Applied Mathematics,
-%               The Hong Kong Polytechnic University
-%
 %   Dedicated to late Professor M. J. D. Powell FRS (1936--2015).
-%   ***********************************************************************
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Attribute: public (can  be called directly by users)
--- matlab/interfaces/newuoa.m
+++ matlab/interfaces/newuoa.m
@@ -158,14 +158,7 @@ function [x, fx, exitflag, output] = newuoa(varargin)
 %
 %   See https://www.libprima.net for more information.
 %
-%   ***********************************************************************
-%   Authors:    Tom M. RAGONNEAU (tom.ragonneau@connect.polyu.hk)
-%               and Zaikun ZHANG (zaikun.zhang@polyu.edu.hk)
-%               Department of Applied Mathematics,
-%               The Hong Kong Polytechnic University.
-%
 %   Dedicated to late Professor M. J. D. Powell FRS (1936--2015).
-%   ***********************************************************************
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Attribute: public (can  be called directly by users)
