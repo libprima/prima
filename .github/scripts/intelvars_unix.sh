7ce8511d43df4c921ffca109babf814dc3463676
zaikunzhang <zaikunzhang@gmail.com>
2023-01-13 11:29:28 +0800

    230113.112928.CST try converting .github/acrtipts as a submodule
    Former-commit-id: d4e64e933ef502b36927a8bf29f9cf84fbad03ef [formerly 66ec83d002a6112f1cc1650396723b397b866ad8]
    Former-commit-id: 205a083fc8482cf0fb215e220b8e8bbca762afe9

--- .github/scripts/intelvars_unix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Usage: bash intelvars_unix.sh
-
-source /opt/intel/oneapi/setvars.sh
-ifort --version
-where ifort
-echo PATH="$PATH":"$(dirname "$(command -v ifort)")" >> "$GITHUB_ENV"
