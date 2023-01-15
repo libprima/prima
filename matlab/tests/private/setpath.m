6ee4ad32928b384d7c41b0880345315b5c113729
zaikunzhang <zaikunzhang@gmail.com>
2023-01-15 23:25:54 +0800

    230115.232554.CST correct add_save_path.m: the pathdef.m under userpath is not loaded by default
    Former-commit-id: dd3ab4a8eb9873859f6a7c3e27bc297f618ae2ad [formerly 3a576dc567c55200e694a530d42f8b9c1b253050]
    Former-commit-id: 4e8972485a0fa1470d8b3c1a90387a5640924c9f

--- matlab/tests/private/setpath.m
+++ matlab/tests/private/setpath.m
@@ -3,7 +3,8 @@ function setpath(path_string)
 path(path_string);
 
 try
-    savepath;
+    sys_pathdef = fullfile(matlabroot(), 'toolbox', 'local', 'pathdef.m');
+    savepath(sys_pathdef);
 catch
     % Do nothing.
 end
