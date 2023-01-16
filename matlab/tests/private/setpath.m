5eee4a2cdd2faa005f01ef4b9313cb7be9bafdd9
zaikunzhang <zaikunzhang@gmail.com>
2023-01-16 08:20:29 +0800

    230116.082029.CST revise setup.m

--- matlab/tests/private/setpath.m
+++ matlab/tests/private/setpath.m
@@ -1,6 +1,8 @@
 function setpath(path_string)
 
 path(path_string);
+orig_warning_state = warning;
+warning('off', 'MATLAB:SavePath:PathNotSaved'); % Maybe we do not have the permission to save path.
 
 try
     sys_pathdef = fullfile(matlabroot(), 'toolbox', 'local', 'pathdef.m');
@@ -8,3 +10,5 @@ try
 catch
     % Do nothing.
 end
+
+warning(orig_warning_state); % Restore the behavior of displaying warnings.
