8c75349546f19e80c46ca10a48db7ec75f81b36c
zaikunzhang <zaikunzhang@gmail.com>
2023-02-13 15:39:50 +0800

    230213.153950.CST remove unused files; update comments

--- fortran/newuoa/update.f90
+++ fortran/newuoa/update.f90
@@ -10,7 +10,7 @@ module update_mod
 !
 ! Started: July 2020
 !
-! Last Modified: Monday, November 28, 2022 PM09:45:06
+! Last Modified: Monday, February 13, 2023 PM02:30:20
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -121,13 +121,21 @@ end if
 end subroutine updatexf
 
 
-subroutine updateq(idz, knew, bmat, moderr, xdrop, zmat, gq, hq, pq)
+subroutine updateq(idz, knew, ximproved, bmat, d, moderr, xdrop, xosav, xpt, zmat, gopt, hq, pq)
 !--------------------------------------------------------------------------------------------------!
-! This subroutine updates [GQ, HQ, PQ] when XPT(:, KNEW) changes from XDROP to XNEW.
-! See Section 4 of the NEWUOA paper.
-! N.B.: XNEW is encoded in [BMAT, ZMAT, IDZ]. We only need BMAT(:, KNEW) instead of the full matrix.
+! This subroutine updates GOPT, HQ, and PQ when XPT(:, KNEW) changes from XDROP to XNEW = XOSAV + D,
+! where XOSAV is the upupdated XOPT, namedly the XOPT before UPDATEXF is called. See Section 4 of
+! the NEWUOA paper.
+! N.B.:
+! 1. XNEW is encoded in [BMAT, ZMAT, IDZ] after UPDATEH being called, and it also equals XPT(:, KNEW)
+! after UPDATEXF being called.
+! 2. Indeed, we only need BMAT(:, KNEW) instead of the entire matrix.
+! 3. In Powell's implementation of NEWUOA, the quadratic model is represented by [GQ, PQ, HQ], where
+! GQ is the gradient of the quadratic model at XBASE. However, Powell implemented BOBYQA and LINCOA
+! without GQ but with GOPT, which is the gradient at XBASE + XOPT. In our implementation, we also
+! use GOPT instead of GQ.
 !--------------------------------------------------------------------------------------------------!
-! List of local arrays (including function-output arrays; likely to be stored on the stack): NONE
+! List of local arrays (including function-output arrays; likely to be stored on the stack): PQINC
 !--------------------------------------------------------------------------------------------------!
 
 ! Generic modules
@@ -135,20 +143,24 @@ use, non_intrinsic :: consts_mod, only : RP, IK, ZERO, DEBUGGING
 use, non_intrinsic :: debug_mod, only : assert
 use, non_intrinsic :: infnan_mod, only : is_finite
 use, non_intrinsic :: linalg_mod, only : r1update, issymmetric
-use, non_intrinsic :: powalg_mod, only : omega_col
+use, non_intrinsic :: powalg_mod, only : omega_col, hess_mul
 
 implicit none
 
 ! Inputs
 integer(IK), intent(in) :: idz
 integer(IK), intent(in) :: knew
+logical, intent(in) :: ximproved
 real(RP), intent(in) :: bmat(:, :) ! BMAT(N, NPT + N)
+real(RP), intent(in) :: d(:) ! D(:)
 real(RP), intent(in) :: moderr
 real(RP), intent(in) :: xdrop(:)  ! XDROP(N)
+real(RP), intent(in) :: xosav(:)  ! XOSAV(N)
+real(RP), intent(in) :: xpt(:, :)  ! XPT(N, NPT)
 real(RP), intent(in) :: zmat(:, :)  ! ZMAT(NPT, NPT - N - 1)
 
 ! In-outputs
-real(RP), intent(inout) :: gq(:)    ! GQ(N)
+real(RP), intent(inout) :: gopt(:)  ! GOPT(N)
 real(RP), intent(inout) :: hq(:, :) ! HQ(N, N)
 real(RP), intent(inout) :: pq(:)    ! PQ(NPT)
 
@@ -156,9 +168,10 @@ real(RP), intent(inout) :: pq(:)    ! PQ(NPT)
 character(len=*), parameter :: srname = 'UPDATEQ'
 integer(IK) :: n
 integer(IK) :: npt
+real(RP) :: pqinc(size(pq))
 
 ! Sizes
-n = int(size(gq), kind(n))
+n = int(size(gopt), kind(n))
 npt = int(size(pq), kind(npt))
 
 ! Preconditions
@@ -166,11 +179,14 @@ if (DEBUGGING) then
     call assert(n >= 1 .and. npt >= n + 2, 'N >= 1, NPT >= N + 2', srname)
     call assert(idz >= 1 .and. idz <= size(zmat, 2) + 1, '1 <= IDZ <= SIZE(ZMAT, 2) + 1', srname)
     call assert(knew >= 0 .and. knew <= npt, '0 <= KNEW <= NPT', srname)
+    call assert(knew >= 1 .or. .not. ximproved, 'KNEW >= 1 unless X is not improved', srname)
     call assert(size(xdrop) == n .and. all(is_finite(xdrop)), 'SIZE(XDROP) == N, XDROP is finite', srname)
+    call assert(size(xosav) == n .and. all(is_finite(xosav)), 'SIZE(XOSAV) == N, XOSAV is finite', srname)
     call assert(size(bmat, 1) == n .and. size(bmat, 2) == npt + n, 'SIZE(BMAT)==[N, NPT+N]', srname)
     call assert(issymmetric(bmat(:, npt + 1:npt + n)), 'BMAT(:, NPT+1:NPT+N) is symmetric', srname)
     call assert(size(zmat, 1) == npt .and. size(zmat, 2) == npt - n - 1, &
         & 'SIZE(ZMAT) == [NPT, NPT - N - 1]', srname)
+    call assert(all(is_finite(xpt)), 'XPT is finite', srname)
     call assert(size(hq, 1) == n .and. issymmetric(hq), 'HQ is an NxN symmetric matrix', srname)
 end if
 
@@ -183,7 +199,7 @@ if (knew <= 0) then  ! KNEW < 0 is impossible if the input is correct.
     return
 end if
 
-! The unupdated model corresponding to [GQ, HQ, PQ] interpolates F at all points in XPT except for
+! The unupdated model corresponding to [GOPT, HQ, PQ] interpolates F at all points in XPT except for
 ! XNEW. The error is MODERR = [F(XNEW)-F(XOPT)] - [Q(XNEW)-Q(XOPT)].
 
 ! Absorb PQ(KNEW)*XDROP*XDROP^T into the explicit part of the Hessian.
@@ -192,10 +208,16 @@ call r1update(hq, pq(knew), xdrop)
 pq(knew) = ZERO
 
 ! Update the implicit part of the Hessian.
-pq = pq + moderr * omega_col(idz, zmat, knew)
+pqinc = moderr * omega_col(idz, zmat, knew)
+pq = pq + pqinc
 
-! Update the gradient.
-gq = gq + moderr * bmat(:, knew)
+! Update the gradient, which needs the updated XPT.
+gopt = gopt + moderr * bmat(:, knew) + hess_mul(xosav, xpt, pqinc)
+
+! Further update GOPT if XIMPROVED is TRUE, as XOPT changes from XOSAV to XNEW = XOSAV + D.
+if (ximproved) then
+    gopt = gopt + hess_mul(d, xpt, pq, hq)
+end if
 
 !====================!
 !  Calculation ends  !
@@ -203,7 +225,7 @@ gq = gq + moderr * bmat(:, knew)
 
 ! Postconditions
 if (DEBUGGING) then
-    call assert(size(gq) == n, 'SIZE(GQ) = N', srname)
+    call assert(size(gopt) == n, 'SIZE(GOPT) = N', srname)
     call assert(size(hq, 1) == n .and. issymmetric(hq), 'HQ is an NxN symmetric matrix', srname)
     call assert(size(pq) == npt, 'SIZE(PQ) = NPT', srname)
 end if
@@ -211,63 +233,61 @@ end if
 end subroutine updateq
 
 
-subroutine tryqalt(idz, fval, ratio, bmat, zmat, itest, gq, hq, pq)
+subroutine tryqalt(idz, bmat, fval, ratio, xopt, xpt, zmat, itest, gopt, hq, pq)
 !--------------------------------------------------------------------------------------------------!
 ! This subroutine tests whether to replace Q by the alternative model, namely the model that
 ! minimizes the F-norm of the Hessian subject to the interpolation conditions. It does the
-! replacement if certain criteria are met (i.e., when ITEST = 3). See Section 8 of the NEWUOA paper.
-! N.B.: Indeed, we only need BMAT(:, KNEW) instead of the entire matrix.
-!--------------------------------------------------------------------------------------------------!
-! List of local arrays (including function-output arrays; likely to be stored on the stack):
-! REAL(RP) :: GALT(N)
-! Size of local arrays: REAL(RP)*(N)
+! replacement if certain criteria are met (i.e., when ITEST = 3). See the paragraph around (8.4) of
+! the NEWUOA paper.
 !--------------------------------------------------------------------------------------------------!
 
 ! Generic modules
-use, non_intrinsic :: consts_mod, only : RP, IK, ZERO, TENTH, DEBUGGING
+use, non_intrinsic :: consts_mod, only : RP, IK, ZERO, TEN, TENTH, DEBUGGING
 use, non_intrinsic :: debug_mod, only : assert
 use, non_intrinsic :: infnan_mod, only : is_nan, is_posinf
-use, non_intrinsic :: linalg_mod, only : inprod, matprod, issymmetric
-use, non_intrinsic :: powalg_mod, only : omega_mul
+use, non_intrinsic :: linalg_mod, only : matprod, inprod, issymmetric, trueloc
+use, non_intrinsic :: powalg_mod, only : hess_mul, omega_mul
 
 implicit none
 
 ! Inputs
 integer(IK), intent(in) :: idz
+real(RP), intent(in) :: bmat(:, :)  ! BMAT(N, NPT+N)
 real(RP), intent(in) :: fval(:)     ! FVAL(NPT)
 real(RP), intent(in) :: ratio
-real(RP), intent(in) :: bmat(:, :)  ! BMAT(N, NPT+N)
+real(RP), intent(in) :: xopt(:)     ! XOPT(N)
+real(RP), intent(in) :: xpt(:, :)   ! XOPT(N, NPT)
 real(RP), intent(in) :: zmat(:, :)  ! ZMAT(NPT, NPT-N-1)
 
 ! In-output
 integer(IK), intent(inout) :: itest
-real(RP), intent(inout) :: gq(:)    ! GQ(N)
+real(RP), intent(inout) :: gopt(:)    ! GOPT(N)
 real(RP), intent(inout) :: hq(:, :) ! HQ(N, N)
 real(RP), intent(inout) :: pq(:)    ! PQ(NPT)
 ! N.B.:
-! GQ, HQ, and PQ should be INTENT(INOUT) instead of INTENT(OUT). According to the Fortran 2018
+! GOPT, HQ, and PQ should be INTENT(INOUT) instead of INTENT(OUT). According to the Fortran 2018
 ! standard, an INTENT(OUT) dummy argument becomes undefined on invocation of the procedure.
 ! Therefore, if the procedure does not define such an argument, its value becomes undefined,
-! which is the case for HQ and PQ when ITEST < 3 at exit. In addition, the information in GQ is
+! which is the case for HQ and PQ when ITEST < 3 at exit. In addition, the information in GOPT is
 ! needed for definining ITEST, so it must be INTENT(INOUT).
 
 ! Local variables
 character(len=*), parameter :: srname = 'TRYQALT'
 integer(IK) :: n
 integer(IK) :: npt
-real(RP) :: galt(size(gq))
+real(RP) :: galt(size(gopt))
+real(RP) :: pqalt(size(pq))
 
 ! Debugging variables
 !real(RP) :: intp_tol
 
 ! Sizes
-n = int(size(gq), kind(n))
+n = int(size(gopt), kind(n))
 npt = int(size(pq), kind(npt))
 
 ! Preconditions
 if (DEBUGGING) then
     call assert(n >= 1 .and. npt >= n + 2, 'N >= 1, NPT >= N + 2', srname)
-    call assert(idz >= 1 .and. idz <= size(zmat, 2) + 1, '1 <= IDZ <= SIZE(ZMAT, 2) + 1', srname)
     ! By the definition of RATIO in ratio.f90, RATIO cannot be NaN unless the actual reduction is
     ! NaN, which should NOT happen due to the moderated extreme barrier.
     call assert(.not. is_nan(ratio), 'RATIO is not NaN', srname)
@@ -277,41 +297,35 @@ if (DEBUGGING) then
     call assert(issymmetric(bmat(:, npt + 1:npt + n)), 'BMAT(:, NPT+1:NPT+N) is symmetric', srname)
     call assert(size(zmat, 1) == npt .and. size(zmat, 2) == npt - n - 1, &
         & 'SIZE(ZMAT) == [NPT, NPT - N - 1]', srname)
-    call assert(size(gq) == n, 'SIZE(GQ) = N', srname)
+    call assert(size(gopt) == n, 'SIZE(GOPT) = N', srname)
     call assert(size(hq, 1) == n .and. issymmetric(hq), 'HQ is an NxN symmetric matrix', srname)
     call assert(size(pq) == npt, 'SIZE(PQ) = NPT', srname)
-    ! [GQ, HQ, PQ] cannot pass the following test if FVAL contains extremely large values.
+    ! [GOPT, HQ, PQ] cannot pass the following test if FVAL contains extremely large values.
     !intp_tol = max(1.0E-8_RP, min(1.0E-1_RP, 1.0E10_RP * real(size(pq), RP) * EPS))
-    !call wassert(errquad(fval, xpt, gq, pq, hq) <= intp_tol, 'Q interpolates FVAL at XPT', srname)
+    !call wassert(errquad(fval, xpt, gopt, pq, hq) <= intp_tol, 'Q interpolates FVAL at XPT', srname)
 end if
 
 !====================!
 ! Calculation starts !
 !====================!
 
-! In the NEWUOA paper, Powell replaces Q with Q_alt when RATIO <= 0.01 and ||G_alt|| <= 0.1||GQ||
-! hold for 3 consecutive times (eq(8.4)). But Powell's code compares ABS(RATIO) instead of RATIO
-! with 0.01. Here we use RATIO, which is more efficient as observed in Zaikun ZHANG's PhD thesis
-! (Section 3.3.2). Update (20221121): RATIO > 0.1 seems to work better.
-!if (abs(ratio) > 1.0E-2_RP) then
-!if (ratio > 1.0E-2_RP) then
-!if (.false.) then
-if (ratio > TENTH) then
+! Calculate the parameters of the least Frobenius norm interpolant to the current data.
+pqalt = omega_mul(idz, zmat, fval)
+galt = matprod(bmat(:, 1:npt), fval) + hess_mul(xopt, xpt, pqalt)
+
+! Test whether to replace the new quadratic model by the least Frobenius norm interpolant, making
+! the replacement if the test is satisfied. In the sequel, TEN seems to work a bit better than 100.
+! In addition, Powell checked the magnitude of ABS(RATIO) instead of RATIO.
+! !if (abs(ratio) > 0.01 .or. inprod(gopt, gopt) < 1.0E2_RP * inprod(galt, galt)) then ! Powell's code
+if (ratio > TENTH .or. inprod(gopt, gopt) < TEN * inprod(galt, galt)) then
     itest = 0
 else
-    galt = matprod(bmat(:, 1:npt), fval)
-    if (inprod(gq, gq) < 1.0E2_RP * inprod(galt, galt)) then
-        itest = 0
-    else
-        itest = itest + 1_IK
-    end if
+    itest = itest + 1_IK
 end if
-
-! Replace Q with Q_alt when ITEST >= 3.
 if (itest >= 3) then
-    gq = galt
+    gopt = galt
+    pq = pqalt
     hq = ZERO
-    pq = omega_mul(idz, zmat, fval)
     itest = 0
 end if
 
@@ -321,14 +335,13 @@ end if
 
 ! Postconditions
 if (DEBUGGING) then
-    call assert(size(gq) == n, 'SIZE(GQ) = N', srname)
+    call assert(size(gopt) == n, 'SIZE(GOPT) = N', srname)
     call assert(size(hq, 1) == n .and. issymmetric(hq), 'HQ is an NxN symmetric matrix', srname)
     call assert(size(pq) == npt, 'SIZE(PQ) = NPT', srname)
-    ! [GQ, HQ, PQ] cannot pass the following test if FVAL contains extremely large values.
-    !call wassert(errquad(fval, xpt, gq, pq, hq) <= intp_tol, 'QALT interpolates FVAL at XPT', srname)
+    ! [GOPT, HQ, PQ] cannot pass the following test if FVAL contains extremely large values.
+    !call wassert(errquad(fval, xpt, gopt, pq, hq) <= intp_tol, 'QALT interpolates FVAL at XPT', srname)
 end if
 
 end subroutine tryqalt
-
 
 end module update_mod
