f19c9f25ca53539beae3d93d348e84026a559b83
zaikunzhang <zaikunzhang@gmail.com>
2023-02-15 03:59:25 +0800

    230215.035925.CST correct test_cobyla.f90
    Former-commit-id: 67a01d3331e494adc4024093d41ceb4ead904385 [formerly 5a43cbc24386d0dec78882e95519848110676c4f]
    Former-commit-id: f3d04e82d08a615ffabb4eda593df5e6a6aad4f2

--- fortran/tests/test_cobyla.f90
+++ fortran/tests/test_cobyla.f90
@@ -6,7 +6,7 @@ module test_solver_mod
 !
 ! Started: September 2021
 !
-! Last Modified: Thursday, January 26, 2023 PM12:34:36
+! Last Modified: Wednesday, February 15, 2023 AM03:59:11
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -47,7 +47,7 @@ character(len=PNLEN) :: probs_loc(100)  ! Maximal number of problems to test: 10
 character(len=PNLEN) :: fix_dim_probs(size(probs_loc))  ! Problems with fixed dimensions
 integer :: randseed_loc
 integer :: rseed
-integer(IK), parameter :: bign = 110_IK
+integer(IK), parameter :: bign = 120_IK
 integer(IK) :: dim_list(100)  ! Maximal number of dimensions to test: 100
 integer(IK) :: dimstride_loc
 integer(IK) :: idim
