617d2001f7cbccf1f1aa714e7c0553504fd2cb41
zaikunzhang <zaikunzhang@gmail.com>
2023-03-21 17:25:21 +0800

    230321.172521.CST revise testcu.m
    Former-commit-id: ee09b0fc502d3fc84059952322d29fab6b8c266e [formerly 5abce9c76c1145f2e25d6482625ea24c10394bcc]
    Former-commit-id: 71d7218b2af48dec83f4c038c72d915104e4561e

--- matlab/tests/private/locate_matcutest.m
+++ matlab/tests/private/locate_matcutest.m
@@ -1,6 +1,6 @@
 function cmpaths = locate_matcutest(directory)
-%This function finds where MatCUTEst (https://github.com/equipez/matcutest) is installed, add the
-% paths needed for using MatCUTEst, and return these paths in a cell array.
+%This function finds where MatCUTEst (https://github.com/equipez/matcutest) is installed, adds the
+% paths needed for using MatCUTEst, and returns these paths in a cell array.
 % We search at most 3 levels below the given directory, whose default value is the home directory.
 % N.B.: As of 202301, MatCUTEst supports only Linux.
 
