3e14687657c63e0c6940b8b3512b6aeff8dc2304
zaikunzhang <zaikunzhang@gmail.com>
2023-04-17 23:14:58 +0800

    improve `locate_matcutest`
    Former-commit-id: d408160bc8b4a9acd312e4b7232411c1e882e98e [formerly 94fed47a7155e388c13e5933824ff96d9a88c760]
    Former-commit-id: 5392a37f4ffa082b3996409c997d59d3c26d3d42

--- matlab/tests/private/locate_matcutest.m
+++ matlab/tests/private/locate_matcutest.m
@@ -4,15 +4,30 @@ function cmpaths = locate_matcutest(directory)
 % We search at most 3 levels below the given directory, whose default value is the home directory.
 % N.B.: As of 202301, MatCUTEst supports only Linux.
 
+% We use the following path as the signature to identify MatCUTEst.
+signature_path = fullfile('matcutest', 'mtools', 'src');
+
+% cmtools is the path to the directory containing the signature path.
+cmtools = '';
+
 if nargin < 1
-    directory = homedir();
+    path_strs = strsplit(path(), pathsep);
+    ind = find(endsWith(path_strs, signature_path), 1, 'first');
+    if ~isempty(ind)
+        cmtools = path_strs{ind};
+    else
+        directory = homedir();
+    end
 end
 
-% In the following line, the "*/" before "matcutest" cannot be removed.
-[~, cmtools] = system(['find ', directory, ' -maxdepth 6 -wholename "*/matcutest/mtools/src" -type d -print -quit']);
-
 if isempty(cmtools)
-    error('locate_matcutest:MatCUTEstNotFound', 'MatCUTEst is not found under %s.', directory);
+    % In the following line, the "*/" before signature_path cannot be removed.
+    name_str = ['"*/', signature_path, '"'];
+    [~, cmtools] = system(['find ', directory, ' -maxdepth 6 -wholename ', name_str, ' -type d -print -quit']);
+
+    if isempty(cmtools)
+        error('locate_matcutest:MatCUTEstNotFound', 'MatCUTEst is not found under %s.', directory);
+    end
 end
 
 cmtools = strtrim(cmtools);  % Remove the leading and trailing white-space characters, including '\n'.
