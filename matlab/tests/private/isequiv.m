eaf1bf4af49f59de8562d27efcf61cd7223edfcd
zaikunzhang <zaikunzhang@gmail.com>
2023-05-01 20:28:24 +0800

    fix the definition of WEIGHT in  archiva/230305/COBYLA/GEOMETRY, which led to a failure of B = A^{-1}

--- matlab/tests/private/isequiv.m
+++ matlab/tests/private/isequiv.m
@@ -780,7 +780,7 @@ case 'lincoa'
 case 'cobyla'
     %blacklist = [blacklist, {'LAUNCH', 'MINMAXRB', 'MAKELA1', 'HS75', 'GAUSS3','HATFLDG'}]; % Classical cobyla encounters SEGFAULT
     blacklist = [blacklist, {'HS80'}];  % QRADD_RDIAG: Assertion failed: C^T*Q(:, N) == Rdiag(N).
-    blacklist = [blacklist, {'EXTRASIM', 'HS72', 'POLAK2', 'POLAK6', 'SPIRAL'}]; % Assertion failed: B = A^{-1}
+    blacklist = [blacklist, {'EXTRASIM', 'POLAK2', 'POLAK6', 'SPIRAL'}]; % Assertion failed: B = A^{-1}
     blacklist = [blacklist, {'DEGENLPA'}]; % Classical cobyla encounters infinite cycling
     blacklist = [blacklist, { ...
         'ACOPP30', ...
