d5109b48f6b44f2746e94457003db6e6a091f7ca
zaikunzhang <zaikunzhang@gmail.com>
2023-05-17 19:29:23 +0800

    230517.192923.CST improve `fprint`

--- fortran/common/fprint.f90
+++ fortran/common/fprint.f90
@@ -12,7 +12,7 @@ module fprint_mod
 !
 ! Started: July 2020
 !
-! Last Modified: Wednesday, May 17, 2023 PM06:04:21
+! Last Modified: Wednesday, May 17, 2023 PM07:27:32
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -122,18 +122,16 @@ end if
 ! Print the string.
 ! `WRITE (FUNIT_LOC, '(A)') STRING` would do what we want, but it causes "Buffer overflow on output"
 ! if string is long.This did occur with NAG Fortran Compiler R7.1(Hanzomon) Build 7122. To avoid
-! this problem, we print the string line by line, separated by NEW_LINE('').
+! this problem, we print the string line by line, separated by newlines.
 i = 1
+j = index(string, new_line(''))  ! Index of the first newline in the string.
 slen = len(string)
-do while (.true.)
-    j = index(string(i:slen), new_line(''))
-    if (j == 0) then  ! No more NEW_LINE('') in the string.
-        exit
-    end if
-    write (funit_loc, '(A)') string(i:i + j - 2)
-    i = i + j
+do while (j >= i)  ! J < I: No more newline in the string.
+    write (funit_loc, '(A)') string(i:j - 1)  ! Print the string before the current newline.
+    i = j + 1  ! Index of the character after the current newline.
+    j = i + index(string(i:slen), new_line('')) - 1  ! Index of the next newline.
 end do
-if (string(i:slen) /= '') then
+if (string(i:slen) /= '') then  ! Print the string after the last newline.
     write (funit_loc, '(A)') string(i:slen)
 end if
 
