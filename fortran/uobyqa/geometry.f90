d66dda0b85e251407a4fc679387bb0f10dcb6392
zaikunzhang <zaikunzhang@gmail.com>
2023-05-29 19:07:32 +0800

    230529.190732.CST fix a failure of uobyqa verification

--- fortran/uobyqa/geometry.f90
+++ fortran/uobyqa/geometry.f90
@@ -8,7 +8,7 @@ module geometry_mod
 !
 ! Started: February 2022
 !
-! Last Modified: Wednesday, May 10, 2023 PM08:38:05
+! Last Modified: Monday, May 29, 2023 PM06:53:11
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -269,7 +269,7 @@ gg = sum(g**2)
 ghg = inprod(g, matprod(h, g))
 
 ! Calculate the Cauchy step as a backup. Powell's code does not have this, and D may be 0 or NaN.
-if (gg > 0) then
+if (gg > 0 .and. is_finite(gg)) then
     dcauchy = (delbar / sqrt(gg)) * g
     if (ghg < 0) then
         dcauchy = -dcauchy
