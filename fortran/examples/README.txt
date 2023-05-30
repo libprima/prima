f5cb2108a91a1b5ad46ab15e63efeb9563ecae5e
zaikunzhang <zaikunzhang@gmail.com>
2023-05-30 18:59:09 +0800

    230530.185909.CST updatre Fortran examples

--- fortran/examples/README.txt
+++ fortran/examples/README.txt
@@ -5,6 +5,18 @@ N.B.:
 1. See the Makefiles for how to compile the examples.
 2. The examples assume that the macros in ../common/ppf.h are set to their default values. In
 particular, PRIMA_REAL_PRECISION = 64 (double precision) and PRIMA_INTEGER_KIND = 0 (default integer).
+3. In the Makefiles, we impose Fortran 2018 standard in the compilation. It is our intention to be
+compliant with Fortran 2008 and above.
+4. As of June 2023, the examples run successfully with the following compilers.
+- AMD AOCC Flang 4.0.0
+- Arm Fortran Compiler 22.1
+- Classic Flang 15.0.3
+- GNU gfortran 12.1.0
+- Intel ifort 2021.9.0
+- Intel ifx 2023.1.0
+- NAG Fortran Compiler Release 7.1
+- NVIDIA nvfortran 23.5
+The following discontinued compilers are not supported: Absoft af95, g95, Oracle sunf95.
 
 Coded by Zaikun ZHANG (www.zhangzk.net).
 
--- fortran/examples/uobyqa/uobyqa_example.f90
+++ fortran/examples/uobyqa/uobyqa_example.f90
@@ -5,7 +5,7 @@
 !
 ! Started: July 2020
 !
-! Last Modified: Wednesday, May 03, 2023 PM02:22:34
+! Last Modified: Tuesday, May 30, 2023 PM06:33:27
 !--------------------------------------------------------------------------------------------------!
 
 
@@ -61,20 +61,18 @@ use calfun_mod, only : RP, calfun
 
 implicit none
 
-integer :: i
 integer, parameter :: n = 6
-real(RP) :: x(n)
-real(RP) :: f
-integer :: nf
+integer :: i, nf, info
+real(RP) :: f, x(n)
 
 ! The following lines illustrates how to call the solver to solve the Chebyquad problem.
 x = [(real(i, RP) / real(n + 1, RP), i=1, n)]  ! Starting point
-call uobyqa(calfun, x, f, nf=nf)  ! This call will not print anything.
+call uobyqa(calfun, x, f)  ! This call will not print anything.
 
 ! In addition to the compulsory arguments, the following illustration specifies also RHOBEG and IPRINT,
 ! which are optional. All the unspecified optional arguments (RHOEND, MAXFUN, etc.) will take their
 ! default values coded in the solver.
 x = [(real(i, RP) / real(n + 1, RP), i=1, n)]  ! Starting point
-call uobyqa(calfun, x, f, rhobeg=0.2_RP * x(1), iprint=1, nf=nf)
+call uobyqa(calfun, x, f, rhobeg=0.2_RP * x(1), iprint=1, nf=nf, info=info)
 
 end program uobyqa_exmp
