2f075b3a16c7afc52073a255785d593fa458a226
zaikunzhang <zaikunzhang@gmail.com>
2023-07-18 11:06:26 +0800

    230718.110626.CST pass Aineq, bineq, Aeq, beq, xl, xu to cobyla

--- matlab/mex_gateways/cobyla_mex.F90
+++ matlab/mex_gateways/cobyla_mex.F90
@@ -11,7 +11,7 @@
 !
 ! Started in July 2020
 !
-! Last Modified: Saturday, January 28, 2023 PM11:29:52
+! Last Modified: Tuesday, July 18, 2023 AM10:12:53
 !--------------------------------------------------------------------------------------------------!
 
 #include "fintrf.h"
@@ -21,8 +21,9 @@ subroutine mexFunction(nargout, poutput, nargin, pinput)
 ! This is the entry point to the Fortran MEX function. If the compiled MEX file is named as
 ! FUNCTION_NAME.mex*** (extension depends on the platform), then in MATLAB we can call:
 ! [x, f, cstrv, constr, info, nf, xhist, fhist, chist, conhist] = ...
-!   FUNCTION_NAME(funcon, x0, f0, constr0, rhobeg, rhoend, eta1, eta2, gamma1, gamma2, ftarget, ...
-!   ctol, cweight, maxfun, iprint, maxhist, output_xhist, output_conhist, maxfilt)
+!   FUNCTION_NAME(funcon, x0, f0, constr0, Aineq, bineq, Aeq, beq, lb, ub, rhobeg, rhoend, ...
+!   eta1, eta2, gamma1, gamma2, ftarget, ctol, cweight, maxfun, iprint, maxhist, output_xhist, ...
+!   output_conhist, maxfilt)
 !--------------------------------------------------------------------------------------------------!
 
 ! Generic modules
@@ -45,10 +46,6 @@ mwPointer, intent(in) :: pinput(nargin)
 mwPointer, intent(out) :: poutput(nargout)
 
 ! Local variables
-real(RP) :: eta1
-real(RP) :: eta2
-real(RP) :: gamma1
-real(RP) :: gamma2
 integer(IK) :: info
 integer(IK) :: iprint
 integer(IK) :: m
@@ -59,24 +56,36 @@ integer(IK) :: nf
 logical :: output_conhist
 logical :: output_xhist
 mwPointer :: funcon_ptr
+mwPointer :: funcon_norma_ptr
 real(RP) :: cstrv
 real(RP) :: ctol
 real(RP) :: cweight
+real(RP) :: eta1
+real(RP) :: eta2
 real(RP) :: f
 real(RP) :: f0
 real(RP) :: ftarget
+real(RP) :: gamma1
+real(RP) :: gamma2
 real(RP) :: rhobeg
 real(RP) :: rhoend
+real(RP), allocatable :: Aeq(:, :)
+real(RP), allocatable :: Aineq(:, :)
+real(RP), allocatable :: beq(:)
+real(RP), allocatable :: bineq(:)
 real(RP), allocatable :: chist(:)
 real(RP), allocatable :: conhist(:, :)
 real(RP), allocatable :: constr(:)
 real(RP), allocatable :: constr0(:)
 real(RP), allocatable :: fhist(:)
+real(RP), allocatable :: lb(:)
+real(RP), allocatable :: ub(:)
 real(RP), allocatable :: x(:)
 real(RP), allocatable :: xhist(:, :)
 
 ! Validate the number of arguments
-call fmxVerifyNArgin(nargin, 19)
+!call fmxVerifyNArgin(nargin, 25)
+call fmxVerifyNArgin(nargin, 26)
 call fmxVerifyNArgout(nargout, 10)
 
 ! Verify that input 1 is a function handle; the other inputs will be verified when read.
@@ -84,24 +93,31 @@ call fmxVerifyClassShape(pinput(1), 'function_handle', 'rank0')
 
 ! Read the inputs
 funcon_ptr = pinput(1)  ! FUNCON_PTR is a pointer to the function handle
+funcon_norma_ptr = pinput(26)
 call fmxReadMPtr(pinput(2), x)
 call fmxReadMPtr(pinput(3), f0)
 call fmxReadMPtr(pinput(4), constr0)
-call fmxReadMPtr(pinput(5), rhobeg)
-call fmxReadMPtr(pinput(6), rhoend)
-call fmxReadMPtr(pinput(7), eta1)
-call fmxReadMPtr(pinput(8), eta2)
-call fmxReadMPtr(pinput(9), gamma1)
-call fmxReadMPtr(pinput(10), gamma2)
-call fmxReadMPtr(pinput(11), ftarget)
-call fmxReadMPtr(pinput(12), ctol)
-call fmxReadMPtr(pinput(13), cweight)
-call fmxReadMPtr(pinput(14), maxfun)
-call fmxReadMPtr(pinput(15), iprint)
-call fmxReadMPtr(pinput(16), maxhist)
-call fmxReadMPtr(pinput(17), output_xhist)
-call fmxReadMPtr(pinput(18), output_conhist)
-call fmxReadMPtr(pinput(19), maxfilt)
+call fmxReadMPtr(pinput(5), Aineq)
+call fmxReadMPtr(pinput(6), bineq)
+call fmxReadMPtr(pinput(7), Aeq)
+call fmxReadMPtr(pinput(8), beq)
+call fmxReadMPtr(pinput(9), lb)
+call fmxReadMPtr(pinput(10), ub)
+call fmxReadMPtr(pinput(11), rhobeg)
+call fmxReadMPtr(pinput(12), rhoend)
+call fmxReadMPtr(pinput(13), eta1)
+call fmxReadMPtr(pinput(14), eta2)
+call fmxReadMPtr(pinput(15), gamma1)
+call fmxReadMPtr(pinput(16), gamma2)
+call fmxReadMPtr(pinput(17), ftarget)
+call fmxReadMPtr(pinput(18), ctol)
+call fmxReadMPtr(pinput(19), cweight)
+call fmxReadMPtr(pinput(20), maxfun)
+call fmxReadMPtr(pinput(21), iprint)
+call fmxReadMPtr(pinput(22), maxhist)
+call fmxReadMPtr(pinput(23), output_xhist)
+call fmxReadMPtr(pinput(24), output_conhist)
+call fmxReadMPtr(pinput(25), maxfilt)
 
 ! Get the sizes
 m = int(size(constr0), kind(m))  ! M is a compulsory input of the Fortran code.
@@ -109,22 +125,24 @@ m = int(size(constr0), kind(m))  ! M is a compulsory input of the Fortran code.
 ! Call the Fortran code
 ! There are different cases because XHIST/CONHIST may or may not be passed to the Fortran code.
 if (output_xhist .and. output_conhist) then
-    call cobyla(calcfc, m, x, f, cstrv, constr, f0, constr0, nf, rhobeg, rhoend, ftarget, ctol, &
-        & cweight, maxfun, iprint, eta1, eta2, gamma1, gamma2, &
-        & xhist=xhist, fhist=fhist, chist=chist, conhist=conhist, maxhist=maxhist, &
+    call cobyla(calcfc, calcfc_norma, m, x, f, cstrv, constr, Aineq, bineq, Aeq, beq, lb, ub, &
+        & f0, constr0, nf, rhobeg, rhoend, ftarget, ctol, cweight, maxfun, iprint, eta1, eta2, &
+        & gamma1, gamma2, xhist=xhist, fhist=fhist, chist=chist, conhist=conhist, maxhist=maxhist, &
         & maxfilt=maxfilt, info=info)
 elseif (output_xhist) then
-    call cobyla(calcfc, m, x, f, cstrv, constr, f0, constr0, nf, rhobeg, rhoend, ftarget, ctol, &
-        & cweight, maxfun, iprint, eta1, eta2, gamma1, gamma2, &
-        & xhist=xhist, fhist=fhist, chist=chist, maxhist=maxhist, maxfilt=maxfilt, info=info)
+    call cobyla(calcfc, calcfc_norma, m, x, f, cstrv, constr, Aineq, bineq, Aeq, beq, lb, ub, &
+        & f0, constr0, nf, rhobeg, rhoend, ftarget, ctol, cweight, maxfun, iprint, eta1, eta2, &
+        & gamma1, gamma2, xhist=xhist, fhist=fhist, chist=chist, maxhist=maxhist, maxfilt=maxfilt, &
+        & info=info)
 elseif (output_conhist) then
-    call cobyla(calcfc, m, x, f, cstrv, constr, f0, constr0, nf, rhobeg, rhoend, ftarget, ctol, &
-        & cweight, maxfun, iprint, eta1, eta2, gamma1, gamma2, &
-        & fhist=fhist, chist=chist, conhist=conhist, maxhist=maxhist, maxfilt=maxfilt, info=info)
+    call cobyla(calcfc, calcfc_norma, m, x, f, cstrv, constr, Aineq, bineq, Aeq, beq, lb, ub, &
+        & f0, constr0, nf, rhobeg, rhoend, ftarget, ctol, cweight, maxfun, iprint, eta1, eta2, &
+        & gamma1, gamma2, fhist=fhist, chist=chist, conhist=conhist, maxhist=maxhist, &
+        & maxfilt=maxfilt, info=info)
 else
-    call cobyla(calcfc, m, x, f, cstrv, constr, f0, constr0, nf, rhobeg, rhoend, ftarget, ctol, &
-        & cweight, maxfun, iprint, eta1, eta2, gamma1, gamma2, &
-        & fhist=fhist, chist=chist, maxhist=maxhist, maxfilt=maxfilt, info=info)
+    call cobyla(calcfc, calcfc_norma, m, x, f, cstrv, constr, Aineq, bineq, Aeq, beq, lb, ub, &
+        & f0, constr0, nf, rhobeg, rhoend, ftarget, ctol, cweight, maxfun, iprint, eta1, eta2, &
+        & gamma1, gamma2, fhist=fhist, chist=chist, maxhist=maxhist, maxfilt=maxfilt, info=info)
 end if
 
 ! After the Fortran code, XHIST or CONHIST may not be allocated, because it may not have been passed
@@ -155,6 +173,12 @@ call fmxWriteMPtr(conhist(:, 1:min(int(nf), size(conhist, 2))), poutput(10))
 ! Free memory. Indeed, automatic deallocation would take place.
 deallocate (x) ! Allocated by fmxReadMPtr.
 deallocate (constr0)  ! Allocated by fmxReadMPtr.
+deallocate (Aineq) ! Allocated by fmxReadMPtr.
+deallocate (bineq) ! Allocated by fmxReadMPtr.
+deallocate (Aeq) ! Allocated by fmxReadMPtr.
+deallocate (beq) ! Allocated by fmxReadMPtr.
+deallocate (lb) ! Allocated by fmxReadMPtr.
+deallocate (ub) ! Allocated by fmxReadMPtr.
 deallocate (constr)  ! Allocated by the solver
 deallocate (xhist)  ! Allocated by the solver
 deallocate (fhist)  ! Allocated by the solver
@@ -177,5 +201,19 @@ real(RP), intent(out) :: f_sub
 real(RP), intent(out) :: constr_sub(:)
 call evalcb(funcon_ptr, x_sub, f_sub, constr_sub)
 end subroutine calcfc
+!--------------------------------------------------------------------!
+subroutine calcfc_norma(x_sub, f_sub, constr_sub)
+! This is an internal procedure that defines CALCFC. Since F2008, we
+! can pass internal procedures as actual arguments. See Note 12.18
+! on page 290 of WD 1539-1 J3/10-007r1 (F2008 Working Document).
+! We implement CALCFC internally so that FUNCON_PTR is visible to it.
+! Do NOT pass FUNCON_PTR by a module variable, which is thread-unsafe.
+use, non_intrinsic :: cbfun_mod, only : evalcb
+implicit none
+real(RP), intent(in) :: x_sub(:)
+real(RP), intent(out) :: f_sub
+real(RP), intent(out) :: constr_sub(:)
+call evalcb(funcon_norma_ptr, x_sub, f_sub, constr_sub)
+end subroutine calcfc_norma
 !--------------------------------------------------------------------!
 end subroutine mexFunction
