5ccb6c9bff0f9db3b41d2e01455fa1206ed729c6
zaikunzhang <zaikunzhang@gmail.com>
2023-10-06 20:32:04 +0800

    231006.203204.HKT revise profile_infnan

--- fortran/tests/testsuite/inf.F90
+++ fortran/tests/testsuite/inf.F90
@@ -9,10 +9,10 @@ module inf_mod
 !
 ! Started: July 2020.
 !
-! Last Modified: Friday, October 06, 2023 AM11:14:18
+! Last Modified: Friday, October 06, 2023 PM08:29:32
 !--------------------------------------------------------------------------------------------------!
 
-use, non_intrinsic :: huge_mode, only : huge_value
+use, non_intrinsic :: huge_mod, only : huge_value
 implicit none
 private
 public :: is_finite, is_posinf, is_neginf, is_inf
