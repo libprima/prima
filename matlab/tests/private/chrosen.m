60314a14878c62704abc587073abb92940feb197
zaikunzhang <zaikunzhang@gmail.com>
2023-10-15 14:52:21 +0800

    231015.145221.HKT restore chrosen.m

--- matlab/tests/private/chrosen.m
+++ matlab/tests/private/chrosen.m
@@ -1,5 +1,4 @@
 function [f, g] = chrosen(x) % Chained Rosenbrock function
-a = randn(500,500); cond(a);
 alpha = 4.0;
 f = sum((x(2:end)-1).^2 + alpha*(x(2:end).^2 - x(1:end-1)).^2);
 if nargout >= 2
