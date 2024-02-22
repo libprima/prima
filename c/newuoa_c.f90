37e329344d3143dcf7b6659d432edfc6e8d20fb5
zaikunzhang <zaikunzhang@gmail.com>
2024-02-22 10:21:10 +0800

    240222.102110.HKT minor revision to comments

--- c/newuoa_c.f90
+++ c/newuoa_c.f90
@@ -43,9 +43,9 @@ integer(IK) :: nf_loc
 integer(IK), allocatable :: maxfun_loc
 integer(IK), allocatable :: npt_loc
 ! The initialization below to null is necessary to avoid a bug with the newer Intel compiler ifx.
-! See details here: https://fortran-lang.discourse.group/t/strange-issue-with-ifx-compiler-and-assume-recursion/7013
-! The bug was observed in all versions of ifx up to 2024.0.1. Once this bug is fixed we should remove the
-! initialization to null because it implies the 'save' attribute, which is undesirable.
+! See https://fortran-lang.discourse.group/t/strange-issue-with-ifx-compiler-and-assume-recursion/7013
+! The bug was observed in all versions of ifx up to 2024.0.1. Once this bug is fixed we should
+! remove the initialization to null because it implies the 'save' attribute, which is undesirable.
 procedure(CCALLBACK), pointer :: cb_ptr => null()
 procedure(COBJ), pointer :: obj_ptr => null()
 real(RP) :: f_loc
