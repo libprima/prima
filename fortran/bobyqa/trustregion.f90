b79a1b90cf406e4ed8243cbf35931a7c27fbe271
zaikunzhang <zaikunzhang@gmail.com>
2024-03-15 21:40:50 +0800

    240315.214050.HKT fix a failure of vtest

--- fortran/bobyqa/trustregion.f90
+++ fortran/bobyqa/trustregion.f90
@@ -8,7 +8,7 @@ module trustregion_bobyqa_mod
 !
 ! Started: February 2022
 !
-! Last Modified: Tuesday, March 12, 2024 PM07:53:36
+! Last Modified: Friday, March 15, 2024 PM09:34:56
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
