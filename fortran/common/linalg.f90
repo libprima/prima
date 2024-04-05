4bf79ecc228ada52bed09aad086adea8b3add584
zaikunzhang <zaikunzhang@gmail.com>
2024-04-05 12:23:13 +0800

    240405.122313.HKT add a postcondition to `p_norm` in linalg.f90

--- fortran/common/linalg.f90
+++ fortran/common/linalg.f90
@@ -39,7 +39,7 @@ module linalg_mod
 !
 ! Started: July 2020
 !
-! Last Modified: Saturday, March 16, 2024 AM04:06:59
+! Last Modified: Friday, April 05, 2024 PM12:19:24
 !--------------------------------------------------------------------------------------------------!
 
 implicit none
@@ -1930,6 +1930,7 @@ end if
 if (DEBUGGING) then
     call assert(y >= 0 .or. is_nan(sum(abs(x))), 'Y >= 0 unless X contains NaN', srname)
     call assert(y > 0 .or. is_nan(sum(abs(x))) .or. all(abs(x) <= 0), 'Y > 0 unless X contains NaN or is zero', srname)
+    call assert(is_nan(y) .eqv. any(is_nan(x)), 'Y is NaN if and only if X contains NaN', srname)
 end if
 
 end function p_norm
