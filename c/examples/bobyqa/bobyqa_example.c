9415957080e645d8eafa9c05cc7926c1e5ad314b
vcantarella <vcantarella@gmail.com>
2025-06-23 15:43:20 +0200

    added honour_x0 interface for the algorithm bobyqa in the C api

--- c/examples/bobyqa/bobyqa_example.c
+++ c/examples/bobyqa/bobyqa_example.c
@@ -68,5 +68,35 @@ int main(int argc, char * argv[])
     // Free the result
     prima_free_result(&result);
 
-    return success;
+    // for the second part of the example, we will change the initial point close to the upper bound
+    // and set honour_x0 to true
+    double new_x0[2] = {4.4, 4.4};
+
+    // Set up the problem
+    prima_problem_t honour_problem;
+    prima_init_problem(&honour_problem, n);
+    honour_problem.x0 = new_x0;
+    honour_problem.calfun = &fun;
+    // We define an upper bound that will be active in order to demonstrate the usage.
+    honour_problem.xl = xl;
+    honour_problem.xu = xu;
+
+    prima_options_t honour_options;
+    prima_init_options(&honour_options);
+    honour_options.iprint = PRIMA_MSG_EXIT;
+    honour_options.rhoend = 1e-6;
+    honour_options.maxfun = 500*n;
+    honour_options.honour_x0 = true; // change this to false to see the warning of resetting x0
+    honour_options.callback = &callback;
+    // Call the solver again with honour_x0 = true
+    prima_result_t honour_result;
+    const prima_rc_t honour_rc = prima_minimize(PRIMA_BOBYQA, honour_problem, honour_options, &honour_result);
+    
+    // Print the result
+    printf("x* = {%g, %g}, rc = %d, msg = '%s', evals = %d\n", honour_result.x[0], honour_result.x[1], honour_rc, honour_result.message, honour_result.nf);
+
+    // Check the result
+    int honour_success = (fabs(honour_result.x[0] - 4.5) > 2e-2 || fabs(honour_result.x[1] - 4) > 2e-2);
+
+    return honour_success;
 }
