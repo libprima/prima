69685cf377eb217d8b387c4d2e0d7556fdcf3142
zaikunzhang <zaikunzhang@gmail.com>
2025-07-20 12:14:42 -0700

    250720.121442.PDT update locate_matcutest to do deeper search

--- benchmark/rescue_idz/matlab/tests/private/locate_matcutest.m
+++ benchmark/rescue_idz/matlab/tests/private/locate_matcutest.m
@@ -1,7 +1,7 @@
 function cmpaths = locate_matcutest(directory)
 %This function finds where MatCUTEst (https://github.com/matcutest/matcutest) is installed, adds the
 % paths needed for using MatCUTEst, and returns these paths in a cell array.
-% We search at most 3 levels below the given directory, whose default value is the home directory.
+% We search at most 10 levels below the given directory, whose default value is the home directory.
 % N.B.: As of 202301, MatCUTEst supports only Linux.
 
 if nargin < 1
@@ -9,7 +9,7 @@ if nargin < 1
 end
 
 % In the following line, the "*/" before "matcutest" cannot be removed.
-[~, cmtools] = system(['find ', directory, ' -maxdepth 6 -wholename "*/matcutest/mtools/src" -type d -print -quit']);
+[~, cmtools] = system(['find ', directory, ' -maxdepth 13 -wholename "*/matcutest/mtools/src" -type d -print -quit']);
 
 if isempty(cmtools)
     error('locate_matcutest:MatCUTEstNotFound', 'MatCUTEst is not found under %s.', directory);
