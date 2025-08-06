69c285558c8667c0519b2ac0d16c8c26aa902c9b
zaikunzhang <zaikunzhang@gmail.com>
2025-08-06 09:39:03 +0800

    250806.093903.CST [skip ci] let copy_crash_dump_files print the file when verbose is true

--- matlab/tests/copy_crash_dump_files.m
+++ matlab/tests/copy_crash_dump_files.m
@@ -1,5 +1,9 @@
-function cfiles = copy_crash_dump_files(directory)
+function cfiles = copy_crash_dump_files(directory, verbose)
 %This function copies the crash dump files to a specified directory.
+
+if nargin < 2
+    verbose = false;  % Default to not displaying the content of the crash dump files
+end
 
 pc = parcluster();
 pc_dir = pc.JobStorageLocation;
@@ -14,7 +18,10 @@ files = [files; dir(fullfile('C:\Users\*\AppData\Local\Temp', 'matlab_crash_dump
 cfiles = cell(length(files), 1);
 for ifiles = 1 : length(files)
     cfiles{ifiles} = fullfile(files(ifiles).folder, files(ifiles).name);
-    copyfile(fullfile(files(ifiles).folder, files(ifiles).name), directory);
+    copyfile(cfiles{ifiles}, directory);
+    if verbose
+        type(cfiles{ifiles});  % Display the content of the crash dump file
+    end
 end
 
 return
