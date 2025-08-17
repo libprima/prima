c157328a6a5e125bc37429697e7655e446cafec7
zaikunzhang <zaikunzhang@gmail.com>
2025-08-17 13:13:34 +0800

    250817.131334.CST [skip ci] remove `SWAP_SIZE` from test_flang, as it does not solve the problem; modify Makefile.common to receive `TESTSEED`; modify `FFF` in Makefile.common for `flang` to remove `-ftack-arrays`, which causes SEGFAULT when problems are large

--- fortran/tests/test.F90
+++ fortran/tests/test.F90
@@ -6,7 +6,7 @@ program test
 !
 ! Started: September 2021
 !
-! Last Modified: Sunday, March 31, 2024 PM06:41:22
+! Last Modified: Sun 17 Aug 2025 07:48:05 AM CST
 !--------------------------------------------------------------------------------------------------!
 
 #if !defined PRIMA_TESTDIM
@@ -17,11 +17,16 @@ use, non_intrinsic :: datetime_mod, only : year, week
 use, non_intrinsic :: test_solver_mod, only : test_solver
 implicit none
 
-integer :: yw
+integer :: seed
 
-! YW is the random seed for the tests. It is altered weekly to test the solvers as much as possible.
-yw = 100 * modulo(year(), 100) + week()
-print *, 'The random seed is', yw
+! SEED is the random seed for the tests.
+! The default value of SEED below is altered weekly to test the solvers as much as possible.
+seed = 100 * modulo(year(), 100) + week()
+#if defined PRIMA_TESTSEED
+seed = PRIMA_TESTSEED
+#endif
+
+print *, 'The random seed is', seed
 
 ! PRIMA_TESTDIM is the dimension of the test problem. It can be 'small', 'big', or 'large'.
 ! When it is 'small', then `test_solver` also accepts `mindim`, `maxdim`, and `dimstride`
@@ -29,6 +34,6 @@ print *, 'The random seed is', yw
 ! the test is hard coded in `test_*.f90` for each solver.
 print *, 'The test dimension is ', PRIMA_TESTDIM
 
-call test_solver(randseed=yw, testdim=PRIMA_TESTDIM)
+call test_solver(randseed=seed, testdim=PRIMA_TESTDIM)
 
 end program test
