e9b960ed734c5a9a7ce94d9b0433d1dc823311ee
zaikunzhang <zaikunzhang@gmail.com>
2026-02-11 17:36:48 +0100

    260211.173648.CET [skip ci] minor revision

--- fortran/common/preproc.f90
+++ fortran/common/preproc.f90
@@ -6,7 +6,7 @@ module preproc_mod
 !
 ! Started: July 2020
 !
-! Last Modified: Wed 11 Feb 2026 05:28:08 PM CET
+! Last Modified: Wed 11 Feb 2026 05:35:59 PM CET
 !--------------------------------------------------------------------------------------------------!
 
 ! N.B.:
@@ -166,7 +166,7 @@ if (maxfun <= max(0, min_maxfun - 1)) then
     if (maxfun > 0) then
         maxfun = int(min_maxfun, kind(maxfun))
     else  ! We assume that non-positive values of MAXFUN are produced by overflow.
-        maxfun = int(max(min_maxfun, 10**min(range(maxfun), 4)), kind(maxfun))  !!MATLAB: maxfun =  max(min_maxfun, 10^4);
+        maxfun = int(max(min_maxfun, 10**min(4, range(maxfun))), kind(maxfun))  !!MATLAB: maxfun =  max(min_maxfun, 10^4);
         ! N.B.: Do NOT set MAXFUN to HUGE(MAXFUN), as it may cause overflow and infinite cycling
         ! when used as the upper bound of DO loops. This occurred on 20240225 with gfortran 13. See
         ! https://fortran-lang.discourse.group/t/loop-variable-reaching-integer-huge-causes-infinite-loop
